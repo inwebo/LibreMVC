830686776f0282d36f55347400901a16a108b4ea
inwebo <inwebo@gmail.com>
2013-04-29 16:33:58 +0200

    Add Pools, Routing, Instance behavior, Global Modules, Local Modules

--- autoload.php
+++ autoload.php
@@ -3,15 +3,45 @@ include("core/autoloader/class.autoloader.php");
 
 try {
     spl_autoload_register( "\\LibreMVC\\AutoLoader::handler" );
-    \LibreMVC\Autoloader::addPool("core/");
-    new \LibreMCV\Modules\Foo();
+    // Core
+    \LibreMVC\Autoloader::addPool("./core/");
+    // Modules
+    \LibreMVC\Autoloader::addPool("./");
+    \LibreMVC\Autoloader::addPool("sites/_default");
+    set_error_handler('\\LibreMVC\Errors\\ErrorsHandler::add');
 
-    if(php_sapi_name() == 'cli') {
-        echo 'Yeah';
-    }
+    $uri = new \LibreMCV\Http\Uri($_SERVER['REQUEST_URI']);
+    $instance = new \LibreMVC\Instance( \LibreMVC\Http\Context::getUrl() );
+    $paths = $instance->processPattern( \LibreMVC\Files\Config::load("config/paths.ini"),"home",'index' );
 
+    var_dump($instance);
+    var_dump($paths);
+
+    // Route par defaut
+
+    $restRoute = new LibreMCV\Routing\Route();
+    $restRoute->name = "";
+    $restRoute->pattern = trim(dirname($_SERVER["PHP_SELF"]),'/').'[/][:controller][/][:action][/][:id][/]';
+    \LibreMCV\Routing\RoutesCollection::addRoute($restRoute);
+
+    // Router
+    $router = new \LibreMCV\Routing\Router($uri, \LibreMCV\Routing\RoutesCollection::getRoutes(), \LibreMCV\Routing\UriParser\Asserts::load() );
+    $processedRoute = $router->route();
+    var_dump($processedRoute);
+
+    // Routes systemes
+    \LibreMVC\Database\Driver::setup(
+        "route",
+        new \LibreMVC\Database\Driver\SQlite($paths['base_routes'])
+    );
+
+    \LibreMVC\AutoLoader::getAutoload($paths['base_autoload']);
 
 } catch (\Exception $e) {
     $message = time() . ',' . $e->getCode() . ',' . $e->getFile() . ',' . $e->getLine() . ',' . $e->getMessage() . "\n";
     echo $message;
 }
+/*
+if(php_sapi_name() == 'cli') {
+    echo 'Yeah';
+}*/
