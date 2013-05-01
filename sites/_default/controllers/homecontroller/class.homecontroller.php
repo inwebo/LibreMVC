42936a08d70d5f29bf4e09602f5471d250eeae04
inwebo <inwebo@gmail.com>
2013-05-01 20:36:25 +0200

    Add debug action

--- sites/_default/controllers/homecontroller/class.homecontroller.php
+++ sites/_default/controllers/homecontroller/class.homecontroller.php
@@ -10,19 +10,35 @@ class HomeController extends StandardController {
         parent::__construct();
     }
 
-    public function index() {
-        echo 'l ' . __LINE__ . ' : ' . __FILE__ . '<br>';
+    public function indexAction() {
+        Views\Template\ViewBag::get()->demoViewBag = "Depuis le viewbag !";
         Views::renderAction();
     }
 
-    public function test($a) {
+    public function testAction($a) {
         echo 'l ' . __LINE__ . ' : ' . __FILE__ . '<br>';
         echo 'Home controller testAction with ' . $a . ' parameters';
+
         Views::renderAction();
     }
 
     public function pouetAction() {
         echo 'l ' . __LINE__ . ' : ' . __FILE__ . '<br>';
+        Views::renderAction();
+    }
+
+    public function debugAction() {
+        Views\Template\ViewBag::get()->instance = new \LibreMVC\Instance( \LibreMVC\Http\Context::getUrl() );
+
+        $instance = new \LibreMVC\Instance( \LibreMVC\Http\Context::getUrl() );
+
+        $paths = $instance->processPattern( \LibreMVC\Files\Config::load( "config/paths.ini" ), "home", 'index' );
+        Views\Template\ViewBag::get()->paths = $paths;
+
+        $router = new \LibreMCV\Routing\Router( \LibreMCV\Http\Uri::current(), \LibreMCV\Routing\RoutesCollection::getRoutes(), \LibreMCV\Routing\UriParser\Asserts::load() );
+        Views\Template\ViewBag::get()->router = $router;
+        Views\Template\ViewBag::get()->routedRoute = $router->dispatch();
+
         Views::renderAction();
     }
 
