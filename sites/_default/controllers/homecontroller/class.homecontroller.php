44191a36584bd244a33f7c7aef53513567b82c7c
inwebo <inwebo@gmail.com>
2013-05-01 02:26:36 +0200

    Demo

--- sites/_default/controllers/homecontroller/class.homecontroller.php
+++ sites/_default/controllers/homecontroller/class.homecontroller.php
@@ -1,12 +1,28 @@
 <?php
 namespace LibreMCV\Controllers;
 
-use \LibreMVC\Mvc\Controllers\StandardController;
+use \LibreMVC\Mvc\Controllers\StandardController as StandardController;
+use \LibreMVC\Views;
 
 class HomeController extends StandardController {
 
     public function __construct() {
-        echo 'Home controller';
+        parent::__construct();
+    }
+
+    public function indexAction() {
+        echo 'Home controller indexAction';
+        Views::renderAction();
+    }
+
+    public function pouetAction() {
+        echo __NAMESPACE__;
+        Views::renderAction();
+    }
+
+    public function testAction($a) {
+        echo 'Les parmametres de l invocation = '.$a.'<br>';
+        Views::renderAction();
     }
 
 }
