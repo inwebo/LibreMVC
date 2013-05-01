6f90f375039643dd9d453a9af87c031f87d7695f
inwebo <inwebo@gmail.com>
2013-05-01 20:33:37 +0200

    Debug action

---
+++ sites/_default/views/homecontroller/debug.php
@@ -0,0 +1,17 @@
+<h2>Default paths</h2>
+<?php
+    var_dump(\LibreMVC\Views\Template\ViewBag::get()->paths);
+?>
+<h2>Instance</h2>
+<?php
+    var_dump(\LibreMVC\Views\Template\ViewBag::get()->instance);
+?>
+<h2>Router</h2>
+<?php
+var_dump(\LibreMVC\Views\Template\ViewBag::get()->router);
+?>
+<h2>Routed route</h2>
+<?php
+var_dump(\LibreMVC\Views\Template\ViewBag::get()->routedRoute);
+?>
+<h2>Invoker</h2>
