9cb07b8d93f310e344c5f49f99a6393c656990b8
inwebo <inwebo@gmail.com>
2013-05-01 02:27:23 +0200

    @todo

---
+++ core/instance/paths/class.paths.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 01/05/13
+ * Time: 00:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC\Instance;
+
+
+class Paths {
+
+}
