9339e864526b2f8e5b6492b74e355944ae894bcf
inwebo <inwebo@gmail.com>
2013-05-01 02:22:27 +0200

    Introducing namespaces

--- index.php
+++ index.php
@@ -2,54 +2,19 @@
 
 include("autoload.php");
 
-
-// reflection
+//
+//new \LibreMVC\Autoloader\ClassMap('./core/');
 /*
-class foo {
-
-    public $id;
-    private $name;
-
-    public function __construct() {
-
-    }
-
-    public function foo($test, $a, $b) {
-        var_dump( $test.$a.$b);
-    }
-
-    static public function statik() {
-
-    }
-
-    private function bar() {
-
-    }
-
-}
-
-
-try {
-    \LibreMCV\Mvc::invoker('foo', 'foo',array("hello ", " le ", " world"));
-    new \LibreMCV\Controllers\HomeController();
-}
-catch (Exception $e) {
-
-}
+$ns = \LibreMVC\Autoloader\Namespaces::get("LibreMVC\\Modules\\Blog\\Includer");
+echo $ns->className() . "<br>";
+echo (int)$ns->isNamespaced(). "<br>";
+echo $ns->getConstructorPrefix(). "<br>";
+echo $ns->toAbsolute(). "<br>";
+echo $ns->toPath(). "<br>";
+var_dump($ns). "<br>";
+echo '<hr>';
+var_dump($ns->toArray(false)). "<br>";
+echo '<hr>';
+var_dump($ns->toFilePath());
 */
-$ViewBag = \LibreMVC\Views\Template\ViewBag::get();
 
-$ViewBag->title = "Titre d'un paragraff";
-$ViewBag->test = "#Je suis une variable#";
-$ViewBag->array = array('value1', 'value2', 'key'=>'value3');
-$ViewBag->object = new stdClass();
-$ViewBag->object->test = "test";
-$ViewBag->object->test2 = "test2";
-define('CONSTANTE', 'Je suis une constante');
-//var_dump(is_file('template.php'));
-$ViewBag->isTrue =1;
-$ViewBag->isFalse =0;
-$ViewBag->intMin = 4;
-$ViewBag->intMax = 6;
-$parser = new \LibreMVC\Views\Template\Parser('template.php');
-$parser->render();
