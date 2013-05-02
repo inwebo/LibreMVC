ae4e73f069714ad8e2437d1e2b475c8ebea368cb
inwebo <inwebo@gmail.com>
2013-05-03 00:19:56 +0200

    Doit remplacer Objet URI

---
+++ core/http/request/class.request.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 02/05/13
+ * Time: 22:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMCV\Http;
+
+
+class Request {
+
+}
