75562a07ee7b25e1dd928ad50b5c91aef46eb2b5
inwebo <inwebo@gmail.com>
2013-05-07 00:44:06 +0200

    Add benchmark

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
-
+$timeStart = microtime(true);
 
 include("autoload.php");
-include('core/autoloader/classnamespace/class.classnamespace.php');
-
-class AutoloadDecorator extends \FilterIterator {
-
-    protected $extension;
-
-    /**
-     * (PHP 5 &gt;= 5.1.0)<br/>
-     * Check whether the current element of the iterator is acceptable
-     * @link http://php.net/manual/en/filteriterator.accept.php
-     * @return bool true if the current element is acceptable, otherwise false.
-     */
-    public function accept()
-    {
-        // TODO: Implement accept() method.
-    }
-}
+$a = number_format(microtime(true) - $timeStart, 5);
+echo '<hr>'.$a.' s<hr>';
