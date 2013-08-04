2a9649a80c45c889f9551e1ff6fa9c9aae21a76a
inwebo <inwebo@gmail.com>
2013-08-05 00:23:17 +0200

    comments

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 $timeStart = microtime(true);
 include("autoload.php");
-$a = number_format(microtime(true) - $timeStart, 5);
-echo '<hr>'.$a.' s<hr>';
-?>
+//$a = number_format(microtime(true) - $timeStart, 5);
+//echo '<hr>'.$a.' s<hr>';
+/**
+ *
 <code>
-    <pre><?php echo LibreMVC\Views\Template\ViewBag::get()->log; ?><pre>
+<pre><?php echo LibreMVC\Views\Template\ViewBag::get()->log; ?><pre>
 </code>
+ */
+?>
