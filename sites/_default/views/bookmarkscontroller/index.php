ccb0a0cda10247ac58ef226351c671f8b1643051
inwebo <inwebo@gmail.com>
2013-08-05 22:27:44 +0200

    Update

--- sites/_default/views/bookmarkscontroller/index.php
+++ sites/_default/views/bookmarkscontroller/index.php
@@ -1,8 +1,13 @@
+<?php
+
+use LibreMVC\Views\Template\ViewBag;
+
+?>
 <h2>Bookmarks</h2>
 <ul>
     <?php
     $i=1;
-    foreach(\LibreMVC\Core\Views\ViewBag::get()->bookmarks as $k => $value) {
+    foreach(ViewBag::get()->bookmarks as $k => $value) {
         echo '<h2><a href="category/' . $i++ . '' . '">'. $k .'</a></h2><ul>';
         foreach($value as $k => $v) {
             echo '<li><a href="'.$v['url'].'">'.$v['title'].'</a></li>';
