d4b7ed333cae7e5809404075689e050eaf576837
inwebo <inwebo@gmail.com>
2013-08-06 21:35:11 +0200

    Added

---
+++ core/mvc/controllers/restcontroller/restcontroller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 06/08/13
+ * Time: 21:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC\Mvc\Controllers;
+
+
+class RestController {
+
+}
---
+++ todo.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 06/08/13
+ * Time: 21:10
+ * To change this template use File | Settings | File Templates.
+ */
