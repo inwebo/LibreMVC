ea91b2a0de72932a96134e2bd446dd5fae7938b5
inwebo <inwebo@gmail.com>
2013-08-06 21:35:20 +0200

    Added

--- todo.php
+++ todo.php
@@ -6,3 +6,9 @@
  * Time: 21:10
  * To change this template use File | Settings | File Templates.
  */
+
+//@todo Revoir routes ! les [:id] devraient être conformes. http://www.inwebo.dev/LibreMVC/bookmarks/category/1/psage/1 === http://www.inwebo.dev/LibreMVC/bookmarks/category/1/page/1
+//@todo Ajout des routes ajax pour les bookmarks
+//@todo Le client REST doit pouvoir demander un content-type par le header HTTP Accept, les erreurs ajax devraient retournées un object LibreMVC\Errors sérialisé
+//@todo RestController
+//@todo Bookmarks devrait entendre Entity, dans models/ du site bookmarks
