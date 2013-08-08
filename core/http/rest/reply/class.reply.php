6b2bc07f95607cafef393251b70596521fb15f1f
inwebo <inwebo@gmail.com>
2013-08-09 00:06:50 +0200

    Introducing Rest Reply object

---
+++ core/http/rest/reply/class.reply.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 08/08/13
+ * Time: 23:19
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC\Http\Rest;
+
+
+class Reply {
+
+}
