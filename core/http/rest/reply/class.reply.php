3aa3de3196524e948f3e686007b3ab19a210cf9d
inwebo <inwebo@gmail.com>
2013-08-09 00:08:48 +0200

    Introducing reply

--- core/http/rest/reply/class.reply.php
+++ core/http/rest/reply/class.reply.php
@@ -11,5 +11,70 @@ namespace LibreMVC\Http\Rest;
 
 
 class Reply {
+    /**
+     * @var bool
+     */
+    public $valid = true;
+
+    /**
+     * @var mixed
+     */
+    public $msg;
+
+    /**
+     * @var string
+     */
+    public $user;
+
+    /**
+     * @var string
+     */
+    public $token;
+
+    /**
+     * @var string
+     */
+    public $timestamp;
+
+    public function __construct($msg = "", $user = null, $token = null, $timestamp = null) {
+        $this->msg = $msg;
+        $this->user = $user;
+        $this->token = $token;
+        $this->timestamp = $timestamp;
+        return $this;
+    }
+
+    public function toJson() {
+        return json_encode( $this );
+    }
+
+    //@todo
+    public function toXml() {
+        $dom = new \DOMDocument('1.0','UTF-8');
+        $dom->formatOutput = true;
+        $root = $dom->createElement("errors");
+
+        $error = $dom->createElement("error");
+        $errno = $dom->createElement("type", $this->errno);
+        $errstr = $dom->createElement("message", $this->errstr);
+        $errline = $dom->createElement('line', $this->errline);
+        $context = $dom->createElement('context', $this->errcontext);
+
+        $error->appendChild($errno);
+        $error->appendChild($errstr);
+        $error->appendChild($errline);
+        $error->appendChild($context);
+        $root->appendChild($error);
+        $dom->appendChild($root);
+        return $dom->saveXML();
+    }
+
+    public function toHtml() {
+        return $this->msg;
+    }
+
+    public function toText() {
+        return $this->msg;
+    }
 
 }
