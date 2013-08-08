0ced9df5c5c123a629f749f3fd45edb05bd630c2
inwebo <inwebo@gmail.com>
2013-08-09 00:08:56 +0200

    Building

--- core/mvc/controllers/restcontroller/class.restcontroller.php
+++ core/mvc/controllers/restcontroller/class.restcontroller.php
@@ -10,21 +10,149 @@
 namespace LibreMVC\Mvc\Controllers;
 
 use LibreMVC\Http\Context;
+use LibreMVC\Http\Header;
 use LibreMVC\Mvc\Controllers\PageController;
+use LibreMVC\Http\Rest\Reply;
 
 class RestController extends  PageController {
 
+    /**
+     * @var string
+     */
     public $verb;
-    public $accept = array();
-    public $mustBeLogged = true;
-    public $content;
+
+    /**
+     * @var object
+     */
+    public $httpHeader;
+    /**
+     * @var bool
+     */
+    protected $public = true;
+
+    /**
+     * @var string
+     */
+    protected $user;
+
+    /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var string
+     */
+    protected $timestamp;
+
+    /**
+     * @var bool
+     */
+    protected $logged = false;
+
+    protected $httpReply;
 
     public function __construct() {
         $this->verb = Context::getHttpVerb();
+        $this->negotiateHttpContentType();
+        Header::noCache();
+        $this->httpHeader = (object)apache_request_headers();
+        $this->httpReply = new Reply();
+        if($this->isLoginIn()) {
+            $this->user      = $this->httpHeader->user;
+            $this->token     = $this->httpHeader->Token;
+            $this->timestamp = $this->httpHeader->Timestamp;
+            $this->isValidUser();
+        }
+
+
     }
 
-    public function indexAction(){
+    public function indexAction(  ){
+        switch( strtolower($this->verb) ) {
+            case 'get':
+                $this->get();
+                break;
+            case 'post':
+                $this->post();
+                break;
+            case 'update':
+                $this->update();
+                break;
+            case 'delete':
+                $this->delete();
+                break;
+        }
+    }
 
+    public function get() {
+        echo __METHOD__;
+    }
+
+    public function post() {
+        echo __METHOD__;
+    }
+
+    public function update() {
+        echo __METHOD__;
+    }
+
+    public function delete() {
+
+    }
+
+    public function isLoginIn() {
+        return ( isset( $this->httpHeader->User ) && isset( $this->httpHeader->Token ) && isset( $this->httpHeader->Timestamp ));
+    }
+
+    public function isValidUser() {
+
+    }
+
+    public function negotiateHttpContentType() {
+        switch($this->httpHeader->Accept) {
+
+            case 'application/json':
+                Header::json();
+                break;
+
+            case 'text/xml':
+                Header::xml();
+                break;
+
+            case 'text/html':
+                Header::html();
+                break;
+
+            default:
+            case 'text/plain':
+                Header::textPlain();
+                break;
+
+        }
+    }
+
+    public function __destruct() {
+        switch($this->httpHeader->Accept) {
+
+            case 'application/json':
+                echo $this->httpReply->toJson();
+                break;
+
+            case 'text/xml':
+                echo $this->httpReply->toXml();
+                break;
+
+            case 'text/html':
+                echo $this->httpReply->toHtml();
+                break;
+
+            default:
+            case 'text/plain':
+                echo $this->httpReply->toString();
+                break;
+
+        }
     }
 
 }
