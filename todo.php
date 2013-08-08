98c5e4d75f76e088dafa3bea647098c230d489c8
Inwebo <inwebo@gmail.com>
2013-08-08 14:18:35 +0200

    @todo

--- todo.php
+++ todo.php
@@ -13,3 +13,26 @@
 //@todo Bookmarks devrait etendre Entity, dans models/ du site bookmarks
 //@todo API cli
 //@todo framework javascript : objet error comme LibreMVC
+//@todo Framework javascript : Requêtes ajax doivent pouvoir être signées
+//@todo Au boot chargement des meta donnée de la page ou alors creation de nouvelles meta datas si n'existe pas encore en base.
+
+/* Exemple
+$.ajax({
+type: "GET",
+url: "http://192.168.1.112:8080/LibreMVC/bookmark/",
+data: { name: "John", location: "Boston" },
+beforeSend:function(xhr){
+    xhr.setRequestHeader('Timestamp', Date.now());
+    xhr.setRequestHeader('Token', Date.now());
+    xhr.setRequestHeader('User', Date.now());
+},
+
+}).done(function( msg ) {
+console.log( "Data Saved: " + msg );
+});
+
+
+
+
+
+ */
