2e245b0772040f7503879d9f008005070c760565
inwebo <inwebo@gmail.com>
2013-08-09 00:09:02 +0200

    Cleaned

--- index.php
+++ index.php
@@ -1,13 +1,2 @@
 <?php
-$timeStart = microtime(true);
 include("autoload.php");
-//$a = number_format(microtime(true) - $timeStart, 5);
-//echo '<hr>'.$a.' s<hr>';
-/**
- *
-<code>
-<pre><?php echo LibreMVC\Views\Template\ViewBag::get()->log; ?><pre>
-</code>
- */
-?>
-<?php //var_dump(\LibreMVC\Mvc\Environnement::this()); ?>
