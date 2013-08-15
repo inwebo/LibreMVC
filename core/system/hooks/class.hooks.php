50f94cae500e7da496e95db590db824ad14aa600
inwebo <inwebo@gmail.com>
2013-08-16 00:11:42 +0200

    Introducing Hooks system

--- core/system/hooks/class.hooks.php
+++ core/system/hooks/class.hooks.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace LibreMVC\System;
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,7 +11,42 @@
  * @author julien
  */
 class Hooks {
-    
-    public $list;
+
+    protected static $instance;
+
+    public $hooks;
+
+    private function __construct() {
+        $this->hooks = array();
+    }
+
+    public static function get() {
+        if (!isset(self::$instance)) {
+            $c = __CLASS__;
+            self::$instance = new $c;
+        }
+
+        return self::$instance;
+    }
+
+    public function addHook( $hookName, $function = null) {
+        $this->hooks[$hookName][] = $function;
+    }
+
+    public function callHooks($name) {
+        foreach($this->hooks[$name] as $functions) {
+            $functions->__invoke();
+        }
+    }
+
+    public function __set($key, $value) {
+        $this->$key = $value;
+    }
+
+    public function __get($key) {
+        if (isset($this->$key)) {
+            return $this->$key;
+        }
+    }
     
 }
