6ccd4709f2d43fc4eea9190504aee9da5b0d576f
inwebo <inwebo@gmail.com>
2013-08-15 12:19:19 +0200

    Refactoring

--- core/helpers/bench/class.bench.php
+++ core/helpers/bench/class.bench.php
@@ -14,54 +14,42 @@
  * @since      File available since Beta
  * @static
  */
-class Bench {
+class Benchmark {
 
-    /**
-     * Applique une expression régulière sur un sujet et retourne le temps mis
-     * pour l'effectué n fois.
-     * 
-     * @param array $pattern Un tableau d'expressions régulière.
-     * @param string $subject La chaine
-     * @param int $loop Nombre de boulce à faire
-     */
-    public static function Pattern($pattern, $subject, $loop = 200) {
-        $results = array();
-        foreach ($pattern as $pattern) {
-            $timeStart = microtime(true);
-            $nbLoops = $loop;
-            while ($nbLoops--) {
-                preg_match($pattern, $subject);
-            }
-            $results[$pattern] = number_format(microtime(true) - $timeStart, 5);
-        }
-        asort($results, SORT_NUMERIC);
-        self::Results($results);
+    protected $iterations;
+    protected $callback;
+    protected $timeStart;
+    protected $timeEnd;
+    protected $timeElapsed;
+    protected $memoryStart;
+    protected $memory;
+
+    public function __construct( $iterations, $callback ) {
+        $this->memoryStart = memory_get_usage();
+        $this->iterations = $iterations;
+        $this->callback   = $callback;
+        $this->timeStart = microtime();
+        $this->start();
     }
 
-    /**
-     * Affichage des résultats du benchmark par ordre croissant de durée
-     * d'execution. Le résultat le plus rapide sera l'étalon 100%.
-     * 
-     * @param array $arrayResults Un tableau de temps d'execution d'expression
-     * regulière.
-     */
-    public static function Results($arrayResults) {
-        $first = $arrayResults;
-        $shortest = array_shift($first);
-        $memoryStart = memory_get_usage();
-        $buffer = "<ol>";
-        foreach ($arrayResults as $key => $value) {
-            $buffer .= '<li>[' . self::Delta($shortest, $value) . '% - ' . $value . ' s ] : ' . $key . ' </li>';
+    protected function start() {
+        $this->memory      = memory_get_usage() - $this->memoryStart;
+        while( --$this->iterations >= 0  ) {
+            $this->callback->__invoke();
         }
-        $buffer .= "</ol>";
-        echo memory_get_usage() - $memoryStart . " O";
-        echo $buffer;
+        $this->timeEnd     = microtime();
+        $this->timeElapsed = $this->timeEnd - $this->timeStart;
     }
 
-    public static function Delta($shortest, $test) {
-        if ($shortest > 0) {
-            return round($test * 100 / $shortest);
-        }
+    public function getResult() {
+        return $this->timeElapsed;
     }
 
+    public function getMemoryUsage() {
+        return $this->memory;
+    }
+
+    static public function bench( $iterations, $callback ) {
+        return new self( $iterations, $callback );
+    }
 }
