d2704b8c87bfc07265e1f970c8f425a63e500f3a
inwebo <inwebo@gmail.com>
2013-08-16 00:13:10 +0200

    Refactoring

--- sites/_default/views/bookmarkscontroller/category.php
+++ sites/_default/views/bookmarkscontroller/category.php
@@ -1,21 +1,20 @@
 <h2>Bookmarks</h2>
-page :
 <?php
 $total = ViewBag::get()->totalPages;
+$menus = "";
 for($i=1;$i<=$total;$i++) {
-echo '<a href="bookmarks/category/'.ViewBag::get()->categoryId.'/page/'.$i.'">'.$i.'</a> ';
+$menus .= '<a href="bookmarks/category/'.ViewBag::get()->categoryId.'/page/'.$i.'">'.$i.'</a> ';
 }
+echo $menus;
 ?>
-
 <h3>{$categoryName}</h3>
-<dl>
+<ul>
     <?php foreach(ViewBag::get()->bookmarks as $k => $value) { ?>
-    <dt><a href="<?php echo $value['url'] ?>"><?php echo stripcslashes($value['title']) ?></a></dt><dd><?php echo stripcslashes($value['description']) ?></dd>
+        <?php \LibreMVC\Views\Template\ViewBag::get()->bookmark = $value; ?>
+        <?php include(TPL_BOOKMARK); ?>
+    <!--<dt><a href="<?php echo $value['url'] ?>"><?php echo stripcslashes($value['title']) ?></a></dt><dd><?php echo stripcslashes($value['description']) ?></dd>-->
     <?php } ?>
-</dl>
+</ul>
 <?php
-$total = ViewBag::get()->totalPages;
-for($i=1;$i<=$total;$i++) {
-    echo '<a href="bookmarks/category/'.ViewBag::get()->categoryId.'/page/'.$i.'">'.$i.'</a> ';
-}
+echo $menus;
 ?>
