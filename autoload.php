a7f03181bff8aff0fbb0b8987b4534b363a8168d
inwebo <inwebo@gmail.com>
2013-08-16 00:10:02 +0200

    Routes are defined in Modules autoloader

--- autoload.php
+++ autoload.php
@@ -4,50 +4,7 @@ use LibreMVC\Mvc\Environnement;
 try {
     LibreMVC\AutoLoader::instance()->addPool( './core/' );
     spl_autoload_register( "\\LibreMVC\\AutoLoader::handler" );
-
     define('CSV', ';');
-
-    $bookmarks = new \LibreMVC\Routing\Route();
-    $bookmarks->name = "/LibreMVC/hiphop";
-    $bookmarks->pattern = \LibreMVC\Http\Context::getBaseDir( __FILE__, false ) . '/bookmarks/category/1/page/9';
-    $bookmarks->controller = '\LibreMVC\Controllers\BookmarksController';
-    $bookmarks->action = 'category';
-    //@todo serialize
-    $bookmarks->params = array('idCategorie'=>1, 'Page'=>9);
-    \LibreMVC\Routing\RoutesCollection::addRoute($bookmarks);
-
-    $bookmarks = new \LibreMVC\Routing\Route();
-    $bookmarks->name = "";
-    $bookmarks->pattern = \LibreMVC\Http\Context::getBaseDir( __FILE__, false ) . '/bookmarks/category[/][:id|(regex)idCategorie#^([0-9])+$#][/][page][/][:id|(regex)page#^([0-9])+$#]';
-    $bookmarks->controller = '\LibreMVC\Controllers\BookmarksController';
-    $bookmarks->action = 'category';
-    \LibreMVC\Routing\RoutesCollection::addRoute($bookmarks);
-
-    $bookmarks = new \LibreMVC\Routing\Route();
-    $bookmarks->name = "";
-    $bookmarks->pattern = \LibreMVC\Http\Context::getBaseDir( __FILE__, false ) . '/bookmarks[/][:id|page][/]';
-    $bookmarks->controller = '\LibreMVC\Controllers\BookmarksController';
-    $bookmarks->action = 'index';
-    \LibreMVC\Routing\RoutesCollection::addRoute($bookmarks);
-
-    //@todo si il n'y a pas au minimum un asserts facultatif bug
-    //@todo bugfixe léger[]
-    $bookmarks = new \LibreMVC\Routing\Route();
-    $bookmarks->name       = "";
-    $bookmarks->pattern    = \LibreMVC\Http\Context::getBaseDir( __FILE__, false ) . '/bookmark[/][:id|idBookmark]';
-    $bookmarks->controller = '\LibreMVC\Controllers\BookmarkController';
-    $bookmarks->action     = 'index';
-    \LibreMVC\Routing\RoutesCollection::addRoute($bookmarks);
-
-    // Default route
-    //@todo bug sur les routes nommées le chemin base_view n'est pas construit
-    $defaultRoute = new \LibreMVC\Routing\Route();
-    $defaultRoute->name = "/LibreMVC/yeah";
-    $defaultRoute->pattern = \LibreMVC\Http\Context::getBaseDir( __FILE__, false ) . '[/][:action][/][:id][/]';
-    $defaultRoute->controller = '\LibreMVC\Controllers\HomeController';
-    $defaultRoute->action = 'index';
-    \LibreMVC\Routing\RoutesCollection::addRoute($defaultRoute);
-
     new LibreMVC\System\Boot( new \LibreMVC\System\Boot\Steps() );
     //var_dump(LibreMVC\Mvc\Environnement::this());
 } catch (\Exception $e) {
