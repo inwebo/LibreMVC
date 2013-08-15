5336f138a230ae5b1bb6b0775575654fc58985cc
inwebo <inwebo@gmail.com>
2013-08-16 00:09:26 +0200

    Refactoring
    useless stuff

--- sites/_default/db/table.route.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE routes (
-    "name" TEXT DEFAULT (''),
-    "pattern" TEXT UNIQUE DEFAULT ('[/][:action][/]'),
-    "controller" TEXT NOT NULL DEFAULT ('\LibreMVC\Controllers\HomeController'),
-    "action" TEXT NOT NULL DEFAULT ('index')
-);
