50eba599e1c84de48b1250adba686c26f6a64eb6
inwebo <inwebo@gmail.com>
2013-08-15 12:19:33 +0200

    Author became Base

--- core/html/document/head/class.head.php
+++ core/html/document/head/class.head.php
@@ -6,23 +6,22 @@ use LibreMVC\Database\Entity;
 
 class Head extends Entity{
 
-    public $id;
+    //public $id;
     public $uri;
     public $md5;
+    public $base;
+    public $title;
     public $description;
     public $keywords;
-    public $author;
-    public $title;
 
-
-    public function __construct( $uri, $title, $description = '', $keywords = '', $author ="inwebo") {
+    public function __construct( $uri, $title, $description = '', $keywords = '', $base = null) {
         parent::__construct();
         $this->uri = $uri;
         $this->md5 = md5($this->uri);
         $this->title = $title;
         $this->description = $description;
         $this->keywords = $keywords;
-        $this->author = $author;
+        $this->base = $base;
     }
 
 }
