3e8a2bfbd49cd51900fa27c3dbf993efc7251952
inwebo <inwebo@gmail.com>
2013-08-16 00:13:00 +0200

    Refactoring

--- sites/_default/modules/bookmarks/models/category/class.category.php
+++ sites/_default/modules/bookmarks/models/category/class.category.php
@@ -2,16 +2,23 @@
 /**
  * Created by JetBrains PhpStorm.
  * User: inwebo
- * Date: 12/05/13
- * Time: 17:08
+ * Date: 15/08/13
+ * Time: 21:39
  * To change this template use File | Settings | File Templates.
  */
 
 namespace LibreMVC\Modules\Bookmarks\Models;
 
 
-class Category {
+use LibreMVC\Database\Entity;
 
+class Category extends Entity{
 
+    public $id;
+    public $name;
+
+    public function __construct() {
+        parent::__construct();
+    }
 
 }
