ca7b11f8ff95264d5fb5dbe1928dc08c01bd08be
inwebo <inwebo@gmail.com>
2013-08-15 12:23:20 +0200

    Typo

--- sites/_default/autoload.php
+++ sites/_default/autoload.php
@@ -4,5 +4,5 @@ try {
     LibreMVC\AutoLoader::instance()->addPool( './' );
 } catch (\Exception $e) {
     $message = time() . ',' . $e->getCode() . ',' . $e->getFile() . ',' . $e->getLine() . ',' . $e->getMessage() . "\n";
-    echo $message;
+    //echo $message;
 }
