ed0f90d1f870491a1ae9ba7f65f167275d892244
inwebo <inwebo@gmail.com>
2013-08-18 21:54:54 +0200

    refactoring

---
+++ core/localisation/class.localisation.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 18/08/13
+ * Time: 11:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC;
+
+
+class Localisation {
+
+}
---
+++ core/system/singleton/class.singleton.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 18/08/13
+ * Time: 00:16
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC\System;
+
+
+class Singleton {
+
+}
