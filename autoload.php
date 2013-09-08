8f3aed4ed42bd02fb0aacc9ef2b724ba9658314d
inwebo <inwebo@gmail.com>
2013-09-08 23:04:29 +0200

    Add standart.php will contains global helpers fonctions

--- autoload.php
+++ autoload.php
@@ -2,10 +2,13 @@
 include( dirname(__FILE__) . "/core/autoloader/class.autoloader.php" );
 use LibreMVC\Mvc\Environnement;
 try {
+    include('core/standart.php');
     LibreMVC\AutoLoader::instance()->addPool( './core/' );
     spl_autoload_register( "\\LibreMVC\\AutoLoader::handler" );
-    define('CSV', ';');
+
+
     new LibreMVC\System\Boot( new \LibreMVC\System\Boot\Steps() );
+
     //var_dump(LibreMVC\Mvc\Environnement::this());
 } catch (\Exception $e) {
     $message = time() . ',' . $e->getCode() . ',' . $e->getFile() . ',' . $e->getLine() . ',' . $e->getMessage() . "\n";
