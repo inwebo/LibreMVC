93551e74a5cb592cde43f3250b230e677f01bb1d
inwebo <inwebo@gmail.com>
2013-11-01 19:59:16 +0100

    New ORM model
    LESS

---
+++ core/database/entity/imodifiable/class.imodifiable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 13/10/13
+ * Time: 14:25
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC\Database\Entity;
+
+
+interface IModifiable {
+
+}
