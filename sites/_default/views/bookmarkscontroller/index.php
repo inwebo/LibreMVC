bf871a7c799013b247ded07663bba594509bcae7
inwebo <inwebo@gmail.com>
2013-11-04 22:59:09 +0100

    Cleaned
    Suppression module apache.
    Sortie du modules bookmarks du site par default.
    Supression php cli inutiles. Ce seront les étapes de boot qui chargeront
    l'environnement.

--- sites/_default/views/bookmarkscontroller/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-use LibreMVC\Views\Template\ViewBag;
-?>
-<?php
-    foreach(ViewBag::get()->bookmarks->categories as $name => $categorie ) {
-        ViewBag::get()->bookmarks->categories->current = $categorie;
-        include(TPL_BOOKMARK_CATEGORY);
-    }
-?>
