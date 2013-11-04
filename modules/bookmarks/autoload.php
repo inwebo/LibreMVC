adcda4638f824359e8bdf1cf8a3708a53d96e2e2
inwebo <inwebo@gmail.com>
2013-11-04 23:13:12 +0100

    Bookmarks module

--- modules/bookmarks/autoload.php
+++ modules/bookmarks/autoload.php
@@ -12,7 +12,7 @@ define('TPL_BOOKMARK_CATEGORY', __DIR__ . '/views/tpl/category.php');
 //define('MODULE_BOOKMARK_DB_TABLE_PREFFIX', __DIR__ . '/views/tpl/category.php');
 
 Hooks::get()->addHook( 'addDefaultSessionsVars', function (&$array) {
-    $array['1']['lg']="eee";
+    $array['1']['lg']="FR";
     return $array;
 });
 
@@ -22,35 +22,42 @@ Hooks::get()->addHook('prependRoutes', function(){
 
     $bookmarks = new Route();
     $bookmarks->name = "";
-    $bookmarks->pattern = $base_uri . '/bookmarks/category[/][:id|(regex)idCategorie#^([0-9])+$#][/][page][/][:id|(regex)page#^([0-9])+$#]';
+    $bookmarks->pattern = $base_uri . '/category[/][:id|(regex)idCategorie#^([0-9])+$#][/][page][/][:id|(regex)page#^([0-9])+$#]';
     $bookmarks->controller = '\LibreMVC\Modules\Bookmarks\Controllers\BookmarksController';
     $bookmarks->action = 'category';
     RoutesCollection::addRoute($bookmarks);
 
     $bookmarks = new Route();
     $bookmarks->name = "";
-    $bookmarks->pattern = $base_uri . '/bookmarks/tag[/][:id|tag]';
+    $bookmarks->pattern = $base_uri . '/tag[/][:id|tag]';
     $bookmarks->controller = '\LibreMVC\Modules\Bookmarks\Controllers\BookmarksController';
     $bookmarks->action = 'tag';
     RoutesCollection::addRoute($bookmarks);
 
     $bookmarks = new Route();
     $bookmarks->name = "";
-    $bookmarks->pattern = $base_uri . '/bookmarks/tags[/]';
+    $bookmarks->pattern = $base_uri . '/tags[/]';
     $bookmarks->controller = '\LibreMVC\Modules\Bookmarks\Controllers\BookmarksController';
     $bookmarks->action = 'tags';
     RoutesCollection::addRoute($bookmarks);
 
     $bookmarks = new Route();
     $bookmarks->name = "";
-    $bookmarks->pattern = $base_uri . '/bookmarks/form[/]';
+    $bookmarks->pattern = $base_uri . '/form[/]';
     $bookmarks->controller = '\LibreMVC\Modules\Bookmarks\Controllers\BookmarksController';
     $bookmarks->action = 'form';
     RoutesCollection::addRoute($bookmarks);
 
     $bookmarks = new Route();
     $bookmarks->name = "";
-    $bookmarks->pattern = $base_uri.'/bookmarks[/][:id|(int)page][/]';
+    $bookmarks->pattern = $base_uri . '/widget[/]';
+    $bookmarks->controller = '\LibreMVC\Modules\Bookmarks\Controllers\BookmarksController';
+    $bookmarks->action = 'widget';
+    RoutesCollection::addRoute($bookmarks);
+
+    $bookmarks = new Route();
+    $bookmarks->name = "";
+    $bookmarks->pattern = $base_uri.'[/][:id|(int)page][/]';
     $bookmarks->controller = '\LibreMVC\Modules\Bookmarks\Controllers\BookmarksController';
     $bookmarks->action = 'index';
     RoutesCollection::addRoute($bookmarks);
