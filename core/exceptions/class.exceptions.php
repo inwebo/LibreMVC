1e03560410377949f2fc59219b4e86ad800dcf27
inwebo <inwebo@gmail.com>
2013-11-04 23:03:28 +0100

    Introducing custom exceptions

---
+++ core/exceptions/class.exceptions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 03/11/13
+ * Time: 01:28
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC;
+
+
+class Exceptions {
+
+}
