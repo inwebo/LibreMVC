9037f242d04d4b8cc8c83e5daa264948bf9b4ee6
inwebo <inwebo@gmail.com>
2013-11-04 22:59:45 +0100

    Introducing CLI environnement

--- autoload.php
+++ autoload.php
@@ -1,25 +1,31 @@
 <?php
 include( dirname(__FILE__) . "/core/autoloader/class.autoloader.php" );
+include('core/standart.php');
+include('core/helpers.php');
+
 use LibreMVC\Mvc\Environnement;
+use LibreMVC\System\Boot;
+use LibreMVC\System\Boot\Mvc;
+
 try {
-    include('core/standart.php');
-    include('core/helpers.php');
     LibreMVC\AutoLoader::instance()->addPool( './core/' );
     spl_autoload_register( "\\LibreMVC\\AutoLoader::handler" );
 
+    if(php_sapi_name() !== 'cli') {
+        new Boot( new Mvc() );
+    }
+    else {
+        // Boot cli
+    }
 
-    //new LibreMVC\System\Boot( 0);
-    new LibreMVC\System\Boot( new \LibreMVC\System\Boot\Mvc() );
-    //echo \LibreMVC\Http\Context::getServer(true, true);
-    //var_dump(LibreMVC\Mvc\Environnement::this());
+
 } catch (\Exception $e) {
     $message = time() . ',' . $e->getCode() . ',' . $e->getFile() . ',' . $e->getLine() . ',' . $e->getMessage() . "\n";
     //echo $message;
+    //var_dump($e);
+    //echo get_class($e);
     \LibreMVC\Http\Header::serverError();
     \LibreMVC\Views\Template\ViewBag::get()->error = $message;
     include('error500.php');
 }
-/*
-if(php_sapi_name() == 'cli') {
-    echo 'Yeah';
-}*/
+
