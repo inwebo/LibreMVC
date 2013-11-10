7bc7316ab8fddebe9f93f12992e4e98a4dbbdb72
inwebo <inwebo@gmail.com>
2013-11-10 12:04:14 +0100

    Tests

--- prepend.php
+++ prepend.php
@@ -1,9 +1,25 @@
 <?php
+include( dirname(__FILE__) . "/core/autoloader/class.autoloader.php" );
+include('core/standart.php');
+include('core/helpers.php');
+if( isset($_GET['q']) ) {
+    echo $_GET['q'];
+}
+class Get {
 
-class _Get {
+    public function __construct(){
+        return $this;
+    }
+
+    public function __get( $key ) {
+        $this->$key = $_GET[$key].'+';
+        return $this->$key;
+    }
 
 }
 
 class _Post {
 
 }
+$g = new Get();
+echo $g->q;
