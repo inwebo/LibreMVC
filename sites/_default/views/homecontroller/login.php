9f1b0f210ecb3d48a1de8753efdd9b82a3858273
inwebo <inwebo@gmail.com>
2013-11-19 21:11:06 +0100

    Add login page

---
+++ sites/_default/views/homecontroller/login.php
@@ -0,0 +1,9 @@
+<form class="form-signin">
+    <h2 class="form-signin-heading">Please sign in</h2>
+    <input type="text" placeholder="Email address" class="form-control">
+    <input type="password" placeholder="Password" class="form-control">
+    <label class="checkbox">
+        <input type="checkbox" value="remember-me"> Remember me
+    </label>
+    <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+</form>
