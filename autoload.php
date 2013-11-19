2f3aa8644e518dc27ba94d8053a6f00693c8bca9
inwebo <inwebo@gmail.com>
2013-11-19 21:05:41 +0100

    Add
    Boot requirements & a correct error page

--- autoload.php
+++ autoload.php
@@ -1,33 +1,40 @@
 <?php
-
+include( dirname(__FILE__) . "/core/autoloader/class.autoloader.php" );
+include('core/standart.php');
+include('core/helpers.php');
 
 use LibreMVC\Mvc\Environnement;
 use LibreMVC\System\Boot;
 use LibreMVC\System\Boot\Mvc;
+use LibreMVC\AutoLoader;
+use LibreMVC\Views\Template\ViewBag;
+use LibreMVC\Http\Header;
+use LibreMVC\Errors\ErrorsHandler;
+use LibreMVC\Routing\RoutesCollection;
 
 try {
-    LibreMVC\AutoLoader::instance()->addPool( './core/' );
+    AutoLoader::instance()->addPool( './core/' );
     spl_autoload_register( "\\LibreMVC\\AutoLoader::handler" );
+
+
 
     if(php_sapi_name() !== 'cli') {
         //Boot MVC
+
         new Boot( new Mvc() );
+
     }
     else {
         //Boot cli
     }
 
-    new Boot(new Boot\Requirements());
+    new Boot( new Boot\Requirements() );
 
-    var_dump(\LibreMVC\Errors\ErrorsHandler::$stack);
-    if( isset($_GET['q']) ) {
-        echo $_GET['q'];
-    }
+
+
+
 } catch (\Exception $e) {
     $message = time() . ',' . $e->getCode() . ',' . $e->getFile() . ',' . $e->getLine() . ',' . $e->getMessage() . "\n";
-    //echo $message;
-    //var_dump($e);
-    //echo get_class($e);
     \LibreMVC\Http\Header::serverError();
     \LibreMVC\Views\Template\ViewBag::get()->exception = $e;
     include('error500.php');
