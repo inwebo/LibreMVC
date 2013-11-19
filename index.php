0319e7cda8f2f0183758b4f023b220bd12d3a1e5
inwebo <inwebo@gmail.com>
2013-11-19 21:10:25 +0100

    ini set default timezone

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-//include("autoload.php");
 
-include('core/helpers/benchmark/class.benchmark.php');
-use LibreMVC\Helpers\Benchmark;
+ini_set("date.timezone",'UTC');
 
-static $i = 0;
-$a = new Benchmark( 1, function() {
-
-    new StdClass;
-});
-
-echo $a->getElapsedTime() . '<br>';
-//echo $a->getMemoryUsage(). '<br>';
-//$a = microtime();
-//echo $a . '<br>';
-//usleep(10);
-//$b =microtime();
-//echo $b. '<br>';
-//echo floatval($b) - floatval($a). '<br>';
+include( "autoload.php" );
