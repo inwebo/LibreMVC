06c4fea917328d89cd4067c46475b683d7e8ed39
inwebo <inwebo@gmail.com>
2013-12-08 00:23:40 +0100

    Improvement

--- sites/bookmarks.inwebo.dev/views/errorscontroller/error404.php
+++ sites/bookmarks.inwebo.dev/views/errorscontroller/error404.php
@@ -1,1 +1,11 @@
-<p>Sorry ! Page not found.</p>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-container">
+                <h3>Ooops</h3>
+                <p>Sorry ! Page not found.</p>
+            </div>
+        </div>
+    </div>
+</div>
