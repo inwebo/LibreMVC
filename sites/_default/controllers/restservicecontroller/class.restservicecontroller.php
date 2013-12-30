8129f93639b39fe3f4f31af5b48c97d53e99c5d2
inwebo <inwebo@gmail.com>
2013-12-30 21:49:25 +0100

    Test RestController

---
+++ sites/_default/controllers/restservicecontroller/class.restservicecontroller.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inwebo
+ * Date: 30/12/13
+ * Time: 15:39
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace LibreMVC\Controllers;
+
+
+use LibreMVC\Models\User;
+use LibreMVC\Mvc\Controllers\RestController;
+
+class RestServiceController extends RestController {
+
+    public $public = false;
+
+    protected function isValidUser($user, $token){
+        $user = \LibreMVC\Models\User::loadByPublicKey($user,$token);
+        // Est un utilisateur connus.
+        if( !is_null($user) ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public function post() {
+        //echo __METHOD__;
+        if($this->httpReply->valid) {
+            var_dump( $_POST);
+        }
+    }
+
+    public function put() {
+        //echo __METHOD__;
+        if($this->httpReply->valid) {
+            var_dump( $_POST);
+        }
+    }
+
+}
