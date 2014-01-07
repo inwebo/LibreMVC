7e9a242a077f89ff68d74b9c4c8a6367c93c0804
inwebo <inwebo@gmail.com>
2014-01-08 00:22:00 +0100

    Useless stuff

--- core/routing/autoload.php
+++ core/routing/autoload.php
@@ -3,7 +3,6 @@
 include('router/class.router.php');
 include('routescollection/class.routescollection.php');
 include('uriparser/class.uriparser.php');
-include('uriparser/asserts/class.asserts.php');
 include('uriparser/segment/class.segment.php');
 include('route/class.route.php');
 include('uriparser/routeconstraint/class.routeconstraint.php');
