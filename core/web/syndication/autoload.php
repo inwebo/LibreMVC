db3ddc6fcdd726fd52e0c6783cb3075c263f11d4
inwebo <inwebo@gmail.com>
2014-01-17 00:43:08 +0100

    Step

--- core/web/syndication/autoload.php
+++ core/web/syndication/autoload.php
@@ -1,1 +1,5 @@
 <?php
+include('./core/web/syndication/item/class.item.php');
+include('./core/web/syndication/class.syndication.php');
+include('./core/web/syndication/autoload.php');
+include('./core/web/syndication/metas/class.meta.php');
--- core/web/syndication/class.syndication.php
+++ core/web/syndication/class.syndication.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: inwebo
- * Date: 13/01/14
- * Time: 23:01
- * To change this template use File | Settings | File Templates.
- */
 
 namespace LibreMVC\Web\Syndication;
 
 
 class Syndication {
+
+
 
 }
