f3e8504810b5aa403f6a2b1aabb8d0821967ee8c
inwebo <inwebo@gmail.com>
2014-02-04 00:06:07 +0100

    Final

--- core/instance/class.instance.php
+++ core/instance/class.instance.php
@@ -2,52 +2,6 @@
 
 namespace LibreMVC;
 
-
-
-/**
- * LibreMVC
- *
- * LICENCE
- *
- * You are free:
- * to Share ,to copy, distribute and transmit the work to Remix —
- * to adapt the work to make commercial use of the work
- *
- * Under the following conditions:
- * Attribution, You must attribute the work in the manner specified by
- * the author or licensor (but not in any way that suggests that they
- * endorse you or your use of the work).
- *
- * Share Alike, If you alter, transform, or build upon
- * this work, you may distribute the resulting work only under the
- * same or similar license to this one.
- *
- *
- * @category  LibreMVC
- * @package   Http
- * @subpackage Instance
- * @copyright Copyright (c) 2005-2012 Inwebo (http://www.inwebo.net)
- * @license   http://http://creativecommons.org/licenses/by-nc-sa/3.0/
- * @version   $Id:$
- * @link      https://inwebo@github.com/inwebo/My.Sessions.git
- * @since     File available since Beta 01-01-2012
- */
-
-/**
- * Détermine le dossier de travail courant selon une url.
- * 
- * Simple extension de la class PHP PDO
- * @category   LibreMVC
- * @package    Http
- * @subpackage Route
- * @copyright  Copyright (c) 1793-2222 Inwebo Veritas (http://www.inwebo.net)
- * @license    http://framework.zend.com/license   BSD License
- * @version    $Id:$
- * @link       https://github.com/inwebo/Template
- * @since      File available since Beta
- */
-
-use LibreMVC\Files\Config;
 use LibreMVC\Http\Context;
 
 class Instance {
