7685fc9894120a3e15174f70b68bc715812934f2
inwebo <inwebo@gmail.com>
2014-02-04 00:06:14 +0100

    Rename

--- core/mvc/controller/autoload.php
+++ core/mvc/controller/autoload.php
@@ -2,5 +2,5 @@
 include('icontroller/class.icontroller.php');
 include('basecontroller/class.basecontroller.php');
 include('class.controller.php');
-include('pagecontroller/class.cagecontroller.php');
+include('pagecontroller/class.pagecontroller.php');
 include('restcontroller/class.restcontroller.php');
