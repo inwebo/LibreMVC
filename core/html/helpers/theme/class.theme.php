5bb6987d15b203c339360cfedfb9a15b70392218
inwebo <inwebo@gmail.com>
2014-02-06 00:32:41 +0100

    Refactoring

--- core/html/helpers/theme/class.theme.php
+++ core/html/helpers/theme/class.theme.php
@@ -17,96 +17,32 @@ use LibreMVC\System\Hooks;
 
 class Theme {
 
-    public $name;
-    public $url;
-    public $realPath;
-    public $baseUrls;
-    protected $baseUrl;
-    protected $default;
-    protected $filePattern;
-    protected $autoloader = "theme.ini";
-    protected  $autoloaderRealPath;
-    public $config;
+    protected $_paths;
+    protected $_name;
+    protected $_description;
+    protected $_version;
+    protected $_core;
+    protected $_css;
+    protected $_js;
+    protected $_config;
 
-    public $css;
-    public $js;
-
-    public function __construct( $filePattern, $baseUrl , $name = "", $default = "default") {
-        $this->filePattern       = $filePattern;
-        $this->default           = $default;
-        $this->name              = (strlen($name) === 0) ? $this->default : $name;
-        $this->baseUrl           = $baseUrl;
-        $this->url               = $this->getUrl();
-        $this->baseUrls          = $this->getBaseUrls();
-        $this->realPath = Environnement::this()->instance->realPath . "themes/" . $this->name . "/" ;
-        $this->autoloaderRealPath = $this->realPath . $this->autoloader;
-        $this->config = Config::load($this->autoloaderRealPath, true);
-        $this->css = new \StdClass();
-        $this->js = new \StdClass();
-        $this->processConfig();
+    public function __construct( $paths ) {
+        $this->_paths    = $paths;
+        $this->_config   = Config::load($this->autoloaderRealPath, true);
+        $this->_css      = new \StdClass();
+        $this->_js       = new \StdClass();
     }
 
     public function isValidTheme() {
-        return is_file( $this->autoloaderRealPath );
+        return is_file( $this->_paths->theme_realPath_ini );
     }
 
-    protected function getUrl() {
-        $pattern = array('%theme_current%', '%base_url%');
-        $replace = array ($this->name .'/',$this->baseUrl);
-        return str_replace($pattern, $replace , $this->filePattern);
+    public function getCssUrls() {
+
     }
 
-    protected function getBaseUrls() {
-        $baseUrls = new \StdClass();
-        $baseUrls->css = $this->url . "css/";
-        $baseUrls->js = $this->url . "js/";
-        $baseUrls->assets = $this->baseUrl . "assets/";
-        return $baseUrls;
-    }
+    public function getJsUrls() {
 
-    protected function processConfig() {
-        // Frameworks
-        Hooks::get()->callHooks('prependCoreAssets');
-
-        foreach($this->config->Core as $k => $v) {
-            if( substr($k, 0,2) =='js' ) {
-                //echo $this->baseUrls->assets . 'js/' . $v . "<br>";
-                if(is_file(realpath(getcwd()) . "/assets/js/".$v)) {
-                    $url = "<script type='text/javascript' src='". $this->baseUrls->assets . "js/" .$v . "' ></script>" . "\n";
-
-                    $this->js->$k = $url;
-                }
-                //@todo est en ligne ?
-            }
-            elseif(substr($k, 0,3) =='css') {
-                if(is_file(realpath(getcwd()) . "/assets/css/".$v)) {
-                    $url = "<link rel='stylesheet' href='". $this->baseUrls->assets . "css/" .$v  . "' >" . "\n";
-                    $this->css->$k = $url;
-                }
-            }
-        }
-        Hooks::get()->callHooks('appendCoreAssets');
-
-        // instance
-        Hooks::get()->callHooks('prependCss');
-        foreach($this->config->Css as $k => $v) {
-            //echo $this->realPath . "css/" . $v;
-            if(is_file($this->realPath . "css/" . $v)) {
-                $url = "<link rel='stylesheet' href='". $this->baseUrls->css . "".$v .  "' >" . "\n";
-                $this->css->$k = $url;
-            }
-        }
-        Hooks::get()->callHooks('appendCss');
-
-        Hooks::get()->callHooks('prependJs');
-        foreach($this->config->Js as $k => $v) {
-            //echo $this->realPath . "js/" . $v;
-            if(is_file($this->realPath . "js/" . $v)) {
-                $url = "<script type='text/javascript' src='". $this->baseUrls->js . "" .$v . "' ></script>" . "\n";
-                $this->js->$k = $url;
-            }
-        }
-        Hooks::get()->callHooks('appendJs');
     }
 
 }
