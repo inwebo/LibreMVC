8a2f680818ee81173a080d072a10400b02b1a11d
inwebo <inwebo@gmail.com>
2014-02-06 00:34:18 +0100

    autorender true

--- core/view/class.view.php
+++ core/view/class.view.php
@@ -4,6 +4,7 @@ namespace LibreMVC;
 
 use LibreMVC\View\Template;
 use LibreMVC\View\ViewObject;
+use LibreMVC\View\Parser;
 
 /**
  * Class View
@@ -14,7 +15,7 @@ class View {
     public $viewObject;
     protected $_template;
     protected $_parser;
-    protected $_autoRender = false;
+    protected $_autoRender = true;
 
     public function __construct( $pathToTemplate ) {
         try {
@@ -31,7 +32,7 @@ class View {
     }
 
     public function render() {
-        $this->_parser = new View\Parser($this->_template, $this->viewObject);
+        $this->_parser = new Parser($this->_template, $this->viewObject);
         if( $this->_autoRender ) {
             echo $this->_parser;
         }
