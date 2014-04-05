eb449f47067d7836239a5de5795c596bcb41f954
inwebo <inwebo@inwebo-pc>
2014-04-05 03:10:29 +0200

    Typo

--- core/files/config/class.config.php
+++ core/files/config/class.config.php
@@ -2,46 +2,17 @@
 
 namespace LibreMVC\Files;
 class ConfigFileException extends \Exception {}
-    /**
-     * LibreMVC
-     *
-     * LICENCE
-     *
-     * You are free:
-     * to Share ,to copy, distribute and transmit the work to Remix —
-     * to adapt the work to make commercial use of the work
-     *
-     * Under the following conditions:
-     * Attribution, You must attribute the work in the manner specified by
-     * the author or licensor (but not in any way that suggests that they
-     * endorse you or your use of the work).
-     *
-     * Share Alike, If you alter, transform, or build upon
-     * this work, you may distribute the resulting work only under the
-     * same or similar license to this one.
-     *
-     *
-     * @category  LibreMVC
-     * @package   Files
-     * @subpackage config
-     * @copyright Copyright (c) 2005-2012 Inwebo (http://www.inwebo.net)
-     * @license   http://http://creativecommons.org/licenses/by-nc-sa/3.0/
-     * @version   $Id:$
-     * @link      https://inwebo@github.com/inwebo/My.Sessions.git
-     * @since     File available since Beta 01-01-2012
-     */
 
 /**
- * Parse le fichier config.ini et renvoie les clefs / valeurs.
+
+ */
+
+/**
+ * Class Config
  *
- * @category   LibreMVC
- * @package    Files
- * @subpackage Config
- * @copyright  Copyright (c) 1793-2222 Inwebo Veritas (http://www.inwebo.net)
- * @license    http://framework.zend.com/license   BSD License
- * @version    $Id:$
- * @link       https://github.com/inwebo/Template
- * @since      File available since Beta
+ * Parse le fichier config.ini et renvoie les clefs / valeurs. Is a Multiton.
+ *
+ * @package LibreMVC\Files
  */
 class Config
 {
