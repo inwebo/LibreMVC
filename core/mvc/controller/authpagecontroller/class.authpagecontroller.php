f00d252edae767286ff92fddc9bc3e1bae340e34
inwebo <inwebo@inwebo-pc>
2014-04-06 02:10:14 +0200

    AuthController

--- core/mvc/controller/authpagecontroller/class.authpagecontroller.php
+++ core/mvc/controller/authpagecontroller/class.authpagecontroller.php
@@ -6,13 +6,61 @@
  * Time: 01:24
  */
 
-namespace LibreMVC\Mvc\Controller\PageController;
+namespace LibreMVC\Mvc\Controller;
 
+use LibreMVC\Mvc\Controller;
+use LibreMVC\Sessions;
 
-use LibreMVC\Mvc\Controller\PageController;
+class AuthPageController extends Controller{
 
-class AuthPageController extends PageController{
+    /**
+     * @var bool Test l'authentification sur toutes les actions AVANT leurs execution. Pratique pour les admins
+     */
+    protected $_protectThis = true;
 
+    protected $_user;
+    /**
+     * @var array Si vide tous les roles
+     */
+    protected $_allowedRoles = array();
+    /**
+     * @var array Si vide tous les droits
+     */
+    protected $_allowedPerms = array();
 
+    public function init() {
+        $this->_user = Sessions::get('User');
+        if( $this->_protectThis ) {
+            // Test automatique de l'auth par Role / Perms
+            // Renvoit 404 si echec.
+            
+        }
+    }
+
+    public function userHasRoles($idRoles) {
+        if( !empty( $idRoles ) ) {
+            $allowed = true;
+            foreach($idRoles as $v) {
+                $allowed &= $this->_user->hasRole($v);
+            }
+            return $allowed;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public function userHasPerms($idPerms) {
+        if( !empty( $idPerms ) ) {
+            $allowed = true;
+            foreach($idPerms as $v) {
+                $allowed &= $this->_user->hasPermission($v);
+            }
+            return $allowed;
+        }
+        else {
+            return true;
+        }
+    }
 
 } 
