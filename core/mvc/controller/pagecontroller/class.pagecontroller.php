8f550f85a182e2c1fc3cc6b33ca715f10ca6ea86
inwebo <inwebo@inwebo-pc>
2014-04-06 02:10:51 +0200

    Support meta informations, autosave infos

--- core/mvc/controller/pagecontroller/class.pagecontroller.php
+++ core/mvc/controller/pagecontroller/class.pagecontroller.php
@@ -1,23 +1,32 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: inwebo
- * Date: 01/02/14
- * Time: 23:32
- * To change this template use File | Settings | File Templates.
- */
-
 namespace LibreMVC\Mvc\Controller;
 
 use LibreMVC\Html\Document\Head;
 use LibreMVC\Mvc\Controller;
+use LibreMVC\Database;
 
 class PageController extends Controller {
 
     protected $_head;
+    private $_headClone;
 
     public function init(){
-        $this->_view->vo->_head = new Head();
+        /**
+         * Chargement d'un objet Head.
+         */
+        Head::binder( Database\Provider::get( 'system' ), 'Heads', 'id' );
+        $this->_head = Head::load( md5( $_SERVER['REQUEST_URI'] ),'md5',true);
+        $this->_view->vo->head = $this->_head;
+        $this->_headClone = clone $this->_head;
+        $this->toView('_head', $this->_head );
+
     }
+
+    public function __destruct() {
+        if( $this->_view->vo->head->isEqual($this->_headClone) ) {
+            $this->_view->vo->head->save();
+        }
+    }
+
 
 }
