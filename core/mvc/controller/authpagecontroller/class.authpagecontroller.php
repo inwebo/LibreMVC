fc1482b37aedc69679f911253342579e2ddd9334
inwebo <inwebo@inwebo-pc>
2014-04-06 02:05:17 +0200

    Add Authentificated Controller

---
+++ core/mvc/controller/authpagecontroller/class.authpagecontroller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inwebo
+ * Date: 06/04/14
+ * Time: 01:24
+ */
+
+namespace LibreMVC\Mvc\Controller\PageController;
+
+
+use LibreMVC\Mvc\Controller\PageController;
+
+class AuthPageController extends PageController{
+
+
+
+} 
