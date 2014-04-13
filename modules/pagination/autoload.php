a6bff5317f414c30c3f798fb40620ceddb23cb39
inwebo <inwebo@inwebo-pc>
2014-04-13 22:41:58 +0200

    New module pagination

---
+++ modules/pagination/autoload.php
@@ -0,0 +1,22 @@
+<?php
+LibreMVC\AutoLoader::instance()->addPool( './' );
+
+define('TPL_PAGINATION', __DIR__ . '/views/tpl/pagination.php');
+
+function renderPagination($pagination, $baseUri, $activeClass= "") {
+    $vo = new \LibreMVC\View\ViewObject();
+
+    $vo->min = $pagination->min;
+    $vo->max = $pagination->max;
+    $vo->index = (int)$pagination->index;
+
+    $vo->hasNext = $pagination->next();
+    $vo->next = ($vo->hasNext) ? $pagination->index + 1 : null;
+
+    $vo->hasPrev = $pagination->prev();
+    $vo->prev = ($vo->hasPrev) ? $pagination->index - 1 : null;
+
+    $vo->baseUri = $baseUri;
+    $vo->activeClass = $activeClass;
+    partial(TPL_PAGINATION, $vo);
+}
