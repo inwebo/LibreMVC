68d12c1d83bc4b7160429e25de175b9ab972fdfe
inwebo <inwebo@inwebo-pc>
2014-04-13 22:40:13 +0200

    Clean

--- modules/bookmarks/views/tpl/bookmark.php
+++ modules/bookmarks/views/tpl/bookmark.php
@@ -1,26 +1,12 @@
-<?php
-use LibreMVC\Views\Template\ViewBag;
-?>
-
-<div data-bookmark-id="<?php echo md5( ViewBag::get()->bookmarks->current->url) ?>" data-bookmark-dt="<?php echo ViewBag::get()->bookmarks->current->dt ?>" data-bookmark-id-category="<?php echo ViewBag::get()->bookmarks->current->category ?>">
+<?php // var_dump($this) ?>
+<div data-bookmark-id="<?php echo md5( $this->url) ?>" data-bookmark-dt="<?php echo $this->dt ?>" data-bookmark-id-category="<?php echo $this->category ?>">
     <dt>
-        <!--<img width="16" height="16" src="http://www.inwebo.dev/LibreMVC/assets/img/favicon/<?php echo md5( ViewBag::get()->bookmarks->current->url) ?>.png">-->
-        <a href="<?php echo ViewBag::get()->bookmarks->current->url ?>"><?php echo stripslashes(ViewBag::get()->bookmarks->current->title) ?></a>
-
-        <?php if($_SESSION['User']->login !== 'guest') { ?>
-            <span class="bookmark-panel">
-                                    <span class="label label-danger pull-right"><a class="bookmark-delete" href="#">X</a></span>
-                                    <span class="label label-info pull-right"><a href="/form?verb=UPDATE&user=<?php echo $_SESSION['User']->login ?>&publicKey=<?php
-                                        echo $_SESSION['User']->publicKey
-                                        ?>" class="bookmark-edit">E</a></span>
-                                </span>
-        <?php } ?>
-
-
+        <!--<img width="16" height="16" src="http://www.inwebo.dev/LibreMVC/assets/img/favicon/<?php echo md5( $this->url) ?>.png">-->
+        <a href="<?php echo $this->url ?>"><?php echo stripslashes($this->title) ?></a>
     </dt>
-    <dd><?php echo ViewBag::get()->bookmarks->current->description ?>
+    <dd><?php echo $this->description ?>
         <?php
-            $tags = new \LibreMVC\Modules\Bookmarks\Models\Bookmark\Tags(ViewBag::get()->bookmarks->current->tags);
+            $tags = new \LibreMVC\Modules\Bookmarks\Models\Bookmark\Tags($this->tags);
             $tags = $tags->toArray();
         ?>
         <ul class="">
