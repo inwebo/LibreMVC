e199d90e7111ee99c18e2987b306728109442769
inwebo <inwebo@inwebo-pc>
2014-04-13 22:38:09 +0200

    Final

--- core/mvc/controller/restcontroller/class.restcontroller.php
+++ core/mvc/controller/restcontroller/class.restcontroller.php
@@ -9,6 +9,7 @@
 
 namespace LibreMVC\Mvc\Controller;
 
+use LibreMVC\Models\User;
 use LibreMVC\Mvc\Controller;
 use LibreMVC\Http\Header;
 use LibreMVC\Http\Rest\Reply;
@@ -27,16 +28,41 @@ class RestUser {
 
 }
 
-class RestController extends Controller {
+/**
+ * Class RestController
+ * @package LibreMVC\Mvc\Controller
+ */
+class RestController extends Controller{
 
+    /**
+     * @var bool Le resultat de la requête doit il être mis en cache ?
+     */
     protected $_cachable = false;
+    /**
+     * @var bool Le service nécessite il une requête signée.
+     */
     protected $_public = true;
+    /**
+     * @var \Reply L'objet de réponse
+     */
     protected $_reply;
+    /**
+     * @var User L'utilisateur courant.
+     */
     protected $_user;
 
+    /**
+     * @var string To set
+     * @todo TO SET to prod
+     */
+    protected $_salt = "s";
+
+    public function __construct( Request $request, $view = null ) {
+        $this->_request = $request;
+        $this->init();
+    }
     protected function init() {
         $this->_reply = new Reply();
-
         // Réponse est elle cachable ?
         if( !$this->_cachable ) {
             Header::noCache();
@@ -44,71 +70,55 @@ class RestController extends Controller {
 
         // Est ce un methode privée sans authentification
         if( !$this->_public && !$this->isLoginIn()) {
-            Header::unauthorized();
-            $this->_reply->msg ="Unauthorized acces";
-            return;
+
+            $this->unauthorized();
         }
 
-        $this->_user = new RestUser($this->_request->httpHeader->User, $this->_request->httpHeader->Token, $this->_request->httpHeader->Timestamp);
+        if( !$this->_public ) {
+
+            $this->_user = new RestUser( $this->_request->httpHeader['User'],
+                                         $this->_request->httpHeader['Token'],
+                                         $this->_request->httpHeader['Timestamp']
+            );
+            //var_dump($this->_user);
+
+        }
 
         // Mauvais utilisateur
         if( !$this->_public && !$this->validateRequest() ) {
-            Header::unauthorized();
-            $this->_reply->msg ="Unauthorized acces";
-            return;
+
+            $this->unauthorized();
         }
 
-        // Les actions du controller sont accessibles.
+        $this->negotiateHttpContentType();
+
+        // indexAction disponible
 
     }
 
+    /**
+     * Accés interdit.
+     */
+    protected function unauthorized() {
+        Header::unauthorized();
+        $this->_reply->msg ="Unauthorized acces";
+        return;
+    }
+
+    /**
+     * Est ce une requête signée ?
+     * @return bool
+     */
     public function isLoginIn() {
-        return ( isset( $this->_request->httpHeader->User ) && isset( $this->_request->httpHeader->Token ) && isset( $this->_request->httpHeader->Timestamp ));
+        //var_dump( isset( $this->_request->httpHeader['User'] ) && isset( $this->_request->httpHeader['Token'] ) && isset( $this->_request->httpHeader['Timestamp'] ));
+        return ( isset( $this->_request->httpHeader['User'] ) && isset( $this->_request->httpHeader['Token'] ) && isset( $this->_request->httpHeader['Timestamp'] ));
     }
 
-    public function indexAction(){
-        $args = Environnement::this()->params;
-        switch( $this->verb ) {
-            //@todo options verb
-            case 'options':
-                // https://developer.mozilla.org/fr/docs/HTTP/Access_control_CORS !
-                // X-domain
-                break;
-
-            case 'get':
-                // Peuple variable globale
-                parse_str(file_get_contents('php://input'), $_GET);
-                $this->get($args);
-                break;
-
-            case 'post':
-                // Peuple variable globale
-                parse_str(file_get_contents('php://input'), $_POST);
-                $this->post($args);
-                break;
-
-            case 'update':
-                $this->update($args);
-                break;
-
-            case 'delete':
-                $this->delete($args);
-                break;
-
-            case 'put':
-                parse_str(file_get_contents('php://input'), $_POST);
-                $this->put($args);
-                break;
-        }
-    }
-
-    protected function validateRequest() {
-        // Requête sur les users
-        return true;
-    }
-
+    /**
+     * Definit le ContentTpe du header selon la demande du client.
+     */
     protected function negotiateHttpContentType() {
-        switch($this->_request->httpHeader->Accept) {
+        switch($this->_request->httpHeader['Accept']) {
             case 'application/json':
                 Header::json();
                 break;
@@ -128,8 +138,117 @@ class RestController extends Controller {
         }
     }
 
+    /**
+     * Doit être le point d'entré du controller, 1 controller === une action avec les différents verbes supportés.
+     * Peuple les variables globales.
+     * @
+     * @param $args
+     */
+    public function indexAction($args){
+        switch( strtolower($this->_request->verb ) ) {
+            //@todo options verb
+            case 'options':
+                // https://developer.mozilla.org/fr/docs/HTTP/Access_control_CORS !
+                // X-domain
+                break;
+            /**
+             * ESt inutile recuperation variable depuis le dispatcher
+             */
+            case 'get':
+                // Peuple variable globale
+                parse_str(file_get_contents('php://input'), $_GET);
+                $this->get($args);
+                break;
+
+            case 'post':
+                // Peuple variable globale
+                parse_str(file_get_contents('php://input'), $_POST);
+                $this->post($_POST);
+                break;
+
+            case 'update':
+                $this->update($args);
+                break;
+
+            case 'delete':
+                $this->delete($args);
+                break;
+
+            case 'put':
+                parse_str(file_get_contents('php://input'), $_POST);
+                $this->put($args);
+                break;
+        }
+    }
+
+    public function options() {
+        //echo __METHOD__;
+    }
+
+    public function get($args) {
+        //echo __METHOD__;        var_dump($this);
+    }
+
+    public function post($args) {
+        //echo __METHOD__;
+    }
+
+    public function update($args) {
+        //echo __METHOD__;
+    }
+
+    public function delete($args) {
+        //echo __METHOD__;
+    }
+
+    public function put($args) {
+        //echo __METHOD__;
+    }
+
+    /**
+     * Une requête HTTP est elle valide ?
+     *
+     * @return bool
+     */
+    protected function validateRequest() {
+        $user = User::load($this->_user->user,'login');
+        // L'utilisateur n'existe pas.
+        if(is_null($user)) {
+
+            $this->unauthorized();
+        }
+        else {
+            // Il existe
+            // Et comparaison avec le timestamp est valide
+            if( User::hash( $this->_user->token .
+                            $this->_user->timestamp .
+                            $this->_salt ) === User::hash(
+                                                            $user->publicKey .
+                                                            $this->_user->timestamp .
+                                                            $this->_salt ) ) {
+                // Et sa clef privé est valide.
+                if( User::hashPrivateKey($this->_user->login, $this->_user->token, $user->passPhrase) === $user->privateKey ) {
+                    // Continue
+                    return true;
+                }
+                else {
+
+                    $this->unauthorized();
+                }
+            }
+            else {
+                $this->unauthorized();
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * A la destruction de l'objet affichage de la réponse au format définit dans $this->_request->httpHeader->Accept
+     */
     public function __destruct() {
-        switch($this->_request->httpHeader->Accept) {
+        switch($this->_request->httpHeader['Accept']) {
             case 'application/json':
                 echo $this->_reply->toJson();
                 break;
