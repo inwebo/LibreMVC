449443632896910403b87dd457f830536ee4353b
inwebo <inwebo@inwebo-pc>
2014-07-31 19:53:19 +0200

    Relative pathes

--- core/mvc/autoload.php
+++ core/mvc/autoload.php
@@ -1,10 +1,10 @@
 <?php
 
-include('controllers/class.controllers.php');
-include('controllers/errorscontroller/class.errorscontroller.php');
-include('controllers/frontcontroller/class.frontcontroller.php');
-include('controllers/pagecontroller/class.pagecontroller.php');
-include('controllers/paginatedcontroller/class.paginatedcontroller.php');
-include('controllers/protectedcontroller/class.protectedcontroller.php');
-include('dispatcher/class.dispatcher.php');
-include('environnement/class.environnement.php');
+include('./controllers/class.controllers.php');
+include('./controllers/errorscontroller/class.errorscontroller.php');
+include('./controllers/frontcontroller/class.frontcontroller.php');
+include('./controllers/pagecontroller/class.pagecontroller.php');
+include('./controllers/paginatedcontroller/class.paginatedcontroller.php');
+include('./controllers/protectedcontroller/class.protectedcontroller.php');
+include('./dispatcher/class.dispatcher.php');
+include('./environnement/class.environnement.php');
