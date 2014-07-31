8caaacfc2adda6e1ba0d415d9b23bf89046dfd53
inwebo <inwebo@inwebo-pc>
2014-07-31 19:54:34 +0200

    Cleaned up

--- core/mvc/controller/pagecontroller/class.pagecontroller.php
+++ core/mvc/controller/pagecontroller/class.pagecontroller.php
@@ -10,25 +10,24 @@ class PageController extends Controller {
     protected $_head;
     private $_headClone;
 
-    public function init(){
+    protected function init(){
         /**
          * Chargement d'un objet Head.
          */
         Head::binder( Database\Provider::get( 'system' ), 'Heads', 'id' );
         $this->_head = Head::load( md5( $_SERVER['REQUEST_URI'] ),'md5',true);
-        $this->_view->vo->head = $this->_head;
+        $this->_view->getDataProvider()->head = $this->_head;
         $this->_headClone = clone $this->_head;
         $this->toView('_head', $this->_head );
         $this->toView('_user', $_SESSION['User'] );
-
     }
 
     public function __destruct() {
-        /*
-        if( $this->_view->vo->head->isEqual($this->_headClone) ) {
-            $this->_view->vo->head->save();
+
+        if( $this->_view->getDataProvider()->head->isEqual($this->_headClone) ) {
+            $this->_view->getDataProvider()->head->save();
         }
-        */
+
     }
 
 
