7c38a693e812749e907e48bb5e9b67905f04aac4
inwebo <inwebo@inwebo-pc>
2014-08-02 00:33:00 +0200

    Cleaned !

--- sites/_default/views/bookmarkscontroller/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-//var_dump($this->bookmarks);?>
-<?php renderPagination( $this->pagination , 'bookmarks/', "active"); ?>
-<?php
-foreach($this->bookmarks as $v) { ?>
-    <?php //var_dump(is_file(TPL_BOOKMARK_BOOKMARK)) ?>
-    <?php partial(TPL_BOOKMARK_BOOKMARK, \LibreMVC\View\ViewObject::map($v) ); ?>
-<?php } ?>
-<?php renderPagination( $this->pagination , 'bookmarks/', "active"); ?>
