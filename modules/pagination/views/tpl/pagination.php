3ee83e58435461cdd40196df153ad015c34dd5a1
inwebo <inwebo@inwebo-PC>
2014-09-30 20:14:44 +0200

    Add interval

--- modules/pagination/views/tpl/pagination.php
+++ modules/pagination/views/tpl/pagination.php
@@ -1,10 +1,14 @@
-<?php //var_dump($this) ?>
+<?php
+    $interval = 11;
+    $startIterator = ( $this->index > $interval ) ? $this->index - $interval : 1;
+    $stopIterator = ( ( $this->index + $interval ) < $this->max ) ? $this->index + $interval : $this->max;
+?>
 <ul class="pagination">
     <li><a href="<?php echo $this->baseUri . $this->min; ?>">&laquo;</a></li>
     <?php if( $this->hasPrev ) { ?>
         <li><a href="<?php echo $this->baseUri . $this->prev; ?>">&LeftArrow;</a></li>
     <?php } ?>
-    <?php for($v =1;$v <= $this->max ;$v++) { ?>
+    <?php for($v =$startIterator;$v <= $stopIterator ;$v++) { ?>
     <li class="<?php echo (($v == $this->index) ? $this->activeClass : "") ; ?>"><a href="<?php echo $this->baseUri . $v; ?>" ><?php echo $v ?></a></li>
     <?php } ?>
     <?php if( $this->hasNext ) { ?>
