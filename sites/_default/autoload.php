d62a439a017b51ad6bdb8825cb662787f82cd024
inwebo <inwebo@inwebo-PC>
2014-09-30 20:15:08 +0200

    Routes moved to login module

--- sites/_default/autoload.php
+++ sites/_default/autoload.php
@@ -8,31 +8,11 @@ try {
     $base_uri = getInstanceBaseUri();
 
     // FILO routes
-    // Une route statique n'a pas besoin d'action du controller StaticController, voir __call
-    /*
-    $staticRoute = new Route( $base_uri . '[:static][/]',
-        '\LibreMVC\Controllers\StaticController'
-    );
-    RoutesCollection::get( 'default' )->addRoute( $staticRoute );
-    */
-
     $defaultRoute = new Route( $base_uri . '[:action][/]',
     	'\LibreMVC\Controllers\HomeController',
         'index'
     );
     RoutesCollection::get( 'default' )->addRoute( $defaultRoute );
-
-    $login = new Route( $base_uri . 'log-in',
-        '\LibreMVC\Controllers\LoginController',
-        'login'
-    );
-    RoutesCollection::get( 'default' )->addRoute( $login );
-
-    $logout = new Route( $base_uri . 'log-out',
-        '\LibreMVC\Controllers\LoginController',
-        'logout'
-    );
-    RoutesCollection::get( 'default' )->addRoute( $logout );
 
     $logout = new Route( $base_uri . 'page/[:static][/]',
         '\LibreMVC\Mvc\Controller\StaticController',
