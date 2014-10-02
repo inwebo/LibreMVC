6d223ad38d3771d00bb8d302e4f23881e7bb0e13
inwebo <inwebo@inwebo-PC>
2014-10-02 03:58:08 +0200

    Step

--- modules/bookmarks/assets/js/widget.js
+++ modules/bookmarks/assets/js/widget.js
@@ -1,24 +1,24 @@
 javascript:(function(){
 
-    var user, publicKey, restService, bookmark, xpathFavicon, metas;
+    var user, publicKey, restService, bookmark, xpathFavicon, metas, bookmark, x;
 
     user            = '%user%';
     publicKey       = '%publicKey%';
     restService     = '%restService%';
     xpathFavicon    = document.evaluate('//*[contains(@rel,\'shortcut icon\')]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-    bookmark        = {};
+    bookmark        = new Object();
     bookmark.url    = encodeURIComponent(location.href);
     bookmark.title  = encodeURIComponent(document.title);
     bookmark.favicon= (xpathFavicon.snapshotLength != 0) ? encodeURIComponent(xpathFavicon.snapshotItem(0).getAttribute('href'))  : null;
 
     metas = document.getElementsByTagName('meta');
     var y = metas.length;
-    for (var x=0 ; x<y; x++) {
-        if (metas[x].name.toLowerCase() == 'description') {
+    for (x=0 ; x<y; x++) {
+        if (metas[x].name.toLowerCase() === 'description') {
             bookmark.description = encodeURIComponent(metas[x].content);
         }
-        if (metas[x].name.toLowerCase() == 'keywords') {
+        if (metas[x].name.toLowerCase() === 'keywords') {
             bookmark.keywords = encodeURIComponent(metas[x].content);
         }
     }
