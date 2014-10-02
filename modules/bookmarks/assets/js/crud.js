6295cde9a40eedc0ceaf762d4b8c9a7c13783931
inwebo <inwebo@inwebo-PC>
2014-10-02 03:58:18 +0200

    Crud system

--- modules/bookmarks/assets/js/crud.js
+++ modules/bookmarks/assets/js/crud.js
@@ -1,3 +1,40 @@
-/**
- * Created by inwebo on 02/10/14.
- */
+$(".bookmark-delete").on('click', function(){
+    var bookmark = $(this).closest('div.bookmarks-list');
+
+    var title =  bookmark.first().find('a').first().html();
+    var hash =  bookmark.attr('data-bookmark-id');
+
+    var r = confirm("Delete bookmark, " + title + ' ' + hash);
+
+    if( r ) {
+        $.ajax({
+            type: "DELETE",
+            url: "http://bookmarks.inwebo.dev/api/bookmark/",
+            data:{
+                hash:hash
+            },
+            headers: {
+                Accept : "application/json",
+                "Content-Type": "application/json"
+            },
+            beforeSend:function(xhr){
+                var timestamp = Date.now();
+                xhr.setRequestHeader('User', 'test');
+                xhr.setRequestHeader('Timestamp', timestamp);
+                xhr.setRequestHeader('Token', 'test');
+            }
+        }).error(function(msg){
+            console.log(msg);
+        }).done(function( msg ) {
+            console.log(msg);
+            bookmark.remove();
+        });
+    }
+
+} );
+
+$(".bookmark-edit").on('click', function(){
+    var bookmark = $(this).closest('div.bookmarks-list');
+    var hash =  bookmark.attr('data-bookmark-id');
+    window.open("http://bookmarks.inwebo.dev/api/bookmark/form/" + hash);
+} );
