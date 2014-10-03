8f47c3d7e96a7b3e8a5d823aae3eb323cc3a4e7a
inwebo <inwebo@inwebo-PC>
2014-10-03 15:36:06 +0200

    CV

---
+++ sites/www.julien-hannotin.dev/autoload.php
@@ -0,0 +1,39 @@
+<?php
+use LibreMVC\Routing\RoutesCollection;
+use LibreMVC\Routing\Route;
+
+try {
+    LibreMVC\AutoLoader::instance()->addPool( './' );
+
+    $base_uri = getInstanceBaseUri();
+
+    // FILO routes
+    // Une route statique n'a pas besoin d'action du controller StaticController, voir __call
+    /*
+    $staticRoute = new Route( $base_uri . '[:static][/]',
+        '\LibreMVC\Controllers\StaticController'
+    );
+    RoutesCollection::get( 'default' )->addRoute( $staticRoute );
+    */
+
+    $defaultRoute = new Route( $base_uri . '[:action][/]',
+    	'\LibreMVC\Controllers\HomeController',
+        'index'
+    );
+    RoutesCollection::get( 'default' )->addRoute( $defaultRoute );
+
+    $login = new Route( $base_uri . 'log-in',
+        '\LibreMVC\Controllers\LoginController',
+        'login'
+    );
+    RoutesCollection::get( 'default' )->addRoute( $login );
+
+    $logout = new Route( $base_uri . 'log-out',
+        '\LibreMVC\Controllers\LoginController',
+        'logout'
+    );
+    RoutesCollection::get( 'default' )->addRoute( $logout );
+
+} catch (\Exception $e) {
+    var_dump($e);
+}
---
+++ sites/www.julien-hannotin.dev/views/homecontroller/login.php
@@ -0,0 +1,16 @@
+<?php if($_SESSION['User']->login === 'guest') {?>
+<form class="form-signin" method="post" action="log-in">
+    <h2 class="form-signin-heading">Please sign in</h2>
+    <input name="user" type="text" placeholder="Email address" class="form-control">
+    <input name="password" type="password" placeholder="Password" class="form-control">
+    <label class="checkbox">
+        <input type="checkbox" value="remember-me"> Remember me
+    </label>
+    <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+</form>
+<?php } else {?>
+    <form class="form-signin" method="post" action="log-out">
+        <h2 class="form-signin-heading">Please log out</h2>
+        <button type="submit" class="btn btn-lg btn-primary btn-block">Log out</button>
+    </form>
+<?php } ?>
