953ea2aa2f9abc8ed7fef2bac74388eeba664ebb
inwebo <inwebo@inwebo-PC>
2014-10-07 03:47:37 +0200

    Add requirements
    Mods :
     - mod_rewrite
    
    Extensions :
     - pdo_mydsl
     - pdo_sqlite

--- core/system/boot/requirements/class.requirements.php
+++ core/system/boot/requirements/class.requirements.php
@@ -14,18 +14,61 @@ class Requirements {
 
     const PHP_VERSION = 5.4;
 
+    /**
+     * Extensions
+     *
+     * PDO
+     * pdo_mydsl
+     * pdo_sqlite
+     *
+     */
+
+    /**
+     * Mods
+     *
+     * mod_rewrite
+     */
+
+    /**
+     * Lecture ecriture groupe dans la db/ 0664
+     */
+
     static public function isValidPhpVersion() {
         $version = (float)substr(phpversion(), 0, 3);
         if(!($version >= (float)self::PHP_VERSION)) {
             trigger_error('Require at least php '. self::PHP_VERSION .', you ve got ' . PHP_VERSION);
+            exit;
         }
     }
 
-    static public function isValidCacheDir() {
-        /*$version = (float)substr(phpversion(), 0, 3);
-        if(!($version >= (float)self::PHP_VERSION)) {
-            trigger_error('Require at least php '. self::PHP_VERSION .', you ve got ' . PHP_VERSION);
-        }*/
+    static public function isModRewriteEnable() {
+        if( !function_exists( 'apache_get_modules' ) ) {
+            // Parse chaine
+            ob_start();
+            phpinfo(INFO_MODULES);
+            ob_end_clean();
+            if(!strstr(ob_get_contents,'mod_rewrite')) {
+                trigger_error('Require mod_rewrite ' . PHP_VERSION);
+                exit;
+            }
+        }
+        else {
+            if(!in_array('mod_rewrite',apache_get_modules())) {
+                trigger_error('Require mod_rewrite' . PHP_VERSION);
+                exit;
+            }
+        }
     }
+
+    static public function isPDOSqliteExtensionsEnable() {
+        if( !in_array( 'pdo_sqlite' , get_loaded_extensions() ) ) {
+            trigger_error('Require pdo_sqlite extension' . PHP_VERSION);
+            exit;
+        }
+    }
+
+    //static public function isValidCacheDir() {}
+
+    //static public function isValidDb() {}
 
 }
