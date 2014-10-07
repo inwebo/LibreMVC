f25736b6938648cd665da73dcf34854b142bcf45
inwebo <inwebo@inwebo-PC>
2014-10-07 03:41:16 +0200

    @todo

--- error500.php
+++ error500.php
@@ -1,51 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Oups</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="./themes/default/css/style.css" rel="stylesheet" media="screen">
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="./assets/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <title>HTML5 boilerplate – all you really need…</title>
+    <link rel="stylesheet" href="./assets/css/reset.css">
 </head>
-<body>
-<header>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Oups</a>
-            </div>
-        </div>
-    </div>
-</header>
-<div id="parallax-wrapper">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12"><div class="col-container"><h3><?php echo get_class( \LibreMVC\View\ViewBag::get()->exception ) ?></h3>            <p class="lead">From <?php echo $e->getFile() ?>, line <?php echo $e->getLine() ?></p>
-                    <p><?php echo $e->getMessage() ?></p><p><?php var_dump(LibreMVC\View\ViewBag::get()->exception ) ?></p></div></div>
-        </div>
 
-    </div>
+<body id="home">
 
-
-</div>
-</div>
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10"><div class="col-container"><p>Lorem</p></div></div>
-            <div class="col-md-2"><div class="col-container text-center"><a class="footer-backtotop" href="">TOP</a> </div></div>
-        </div>
-    </div>
-</footer>
+<h1>server error</h1>
+<p>
+    wip !
+</p>
 </body>
 </html>
