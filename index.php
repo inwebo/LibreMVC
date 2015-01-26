3ecbe1040148fd8c7ddca4bf7750a3c3545e76ae
inwebo <inwebo@gmail.com>
2015-01-26 17:06:56 +0100

    Default

--- index.php
+++ index.php
@@ -1,20 +1,2 @@
 <?php
-    ini_set('display_errors', 'on');
-    include('core/autoloader/autoload.php');
-?>
-<!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>Test</title>
-    <base href="http://localhost/libre/" >
-</head>
-<body>
-<h1>LibreMVC 2.0</h1>
-<?php
-$t = glob('tests/*.php');
-foreach($t as $v) { ?>
-    <a href="<?php echo $v ?>"><?php echo $v ?></a><br>
-    <?php } ?>
-</body>
-</html>
+include( "autoload.php" );
