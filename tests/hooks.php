f1f253b91775dc69005ebaadc71b291a7538fc0c
inwebo <inwebo@gmail.com>
2015-01-27 16:53:00 +0100

    Sandbox Hooks system

--- tests/hooks.php
+++ tests/hooks.php
@@ -1,29 +1,114 @@
 <?php
-ini_set('display_errors', 'on');
-include('../core/patterns/isingleton/class.isingleton.php');
-include('../core/system/hooks/autoload.php');
+/**
+ * http://php.net/manual/fr/closure.bind.php
+ * http://php.net/manual/fr/closure.bindto.php
+ * http://www.htmlist.com/development/extending-php-5-3-closures-with-serialization-and-reflection/
+ */
+namespace LibreMVC\System\Hooks\Temp {
 
-use LibreMVC\System\Hooks;
 
-class Foo {
+    class Hook {
+        /**
+         * @var string
+         */
+        protected $_name;
+        /**
+         * @var \ArrayIterator
+         */
+        protected $_callbacks;
 
-    public function __construct(){
-        Hooks::this()->createHook('construct');
-        print 'Hello';
-        Hooks::this()->attachHookCallback('construct', function($s, $t){
-            $t[0] = " ";
-            print  $s . $t[0];
-        });
+        public function __construct($name){
+            $this->_name = $name;
+            $this->_callbacks = new \ArrayIterator();
+        }
+
+        public function getName() {
+            return $this->_name;
+        }
+
+        public function attachCallbacks(CallBack $callback, $priority = null) {
+            if( !is_null($priority) ) {
+                if( is_int($priority) && is_int($priority) > 0 ) {
+                    if( !isset($this->_callbacks[$priority]) ) {
+                        $this->_callbacks[$priority] = $callback;
+                    }
+                    else {
+                        trigger_error("Priority already uesd !");
+                    }
+                }
+            }
+            else {
+                $this->_callbacks[] = $callback;
+            }
+
+        }
+
+        public function call() {
+            $this->_callbacks->rewind();
+            $buffer = array();
+            while( $this->_callbacks->valid() ) {
+                $reflection = new \ReflectionMethod($this->_callbacks->current(), '__invoke');
+                $buffer[] = $reflection->invoke(
+                    $this->_callbacks->current(),
+                    $this->_callbacks->current()->getParameters()
+                );
+                $this->_callbacks->next();
+            }
+            return $buffer;
+            //return $buffer[count($buffer)-1];
+        }
     }
 
-    public function toString(){
-        $arf = ",";
-        $test = array("x");
-        Hooks::this()->callHooks('construct',$arf,$test);
-        print 'world';
+    class CallBack{
+
+        protected $_closure;
+        protected $_reflection;
+
+        public function __construct(\Closure $closure){
+            $this->_closure = $closure;
+            $this->_reflection = new \ReflectionFunction($this->_closure);
+        }
+
+        public function __invoke() {
+            $args = func_get_args();
+            $this->_reflection->invokeArgs($args);
+        }
+
+        public function getParameters() {
+            return $this->_reflection->getParameters();
+        }
+
     }
 
+    $base = "Hello world";
+    echo 'Depart : '. $base . "<br>";
+
+    $callback1 = function($a)use(&$base){
+        echo var_dump($a);
+        $base = "Bonjour le monde";
+    };
+
+    $callback2 = function()use(&$base){
+        $base = "Ola el mundo";
+    };
+
+    $hook = new Hook('test');
+    $hook->attachCallbacks(new CallBack($callback1));
+    $hook->attachCallbacks(new CallBack($callback2));
+    $hook->call();
+    echo 'Fin : '.$base .'<br>';
+
+
+
+
+
+    $base = "Test";
+    $hook = new Hook('test2');
+    $c = new CallBack(function($base){
+        $base = "Fin" . $base[0];
+    });
+
+    $hook->attachCallbacks($c);
+    $hook->call();
+    echo $base .'<br>';
 }
-
-$v = new Foo();
-$v->toString();
