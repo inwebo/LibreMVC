241923f6c076e7069240dd82cfb7fb6b45d4b260
inwebo <inwebo@gmail.com>
2015-01-28 22:05:39 +0100

    Add docs

--- core/mvc/controller/basecontroller/class.basecontroller.php
+++ core/mvc/controller/basecontroller/class.basecontroller.php
@@ -1,51 +1,65 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: inwebo
- * Date: 01/02/14
- * Time: 23:04
- * To change this template use File | Settings | File Templates.
- */
 
 namespace LibreMVC\Mvc\Controller {
     use LibreMVC\Http\Request;
     use LibreMVC\View;
+    use LibreMVC\View\Interfaces\IDataProvider;
 
-    /**
-     * Class BaseController
-     *
-     * Représentation d'un actionController doit posséder une vue ansi qu'une requête.
-     *
-     * @package LibreMVC\Mvc\Controller
-     */
     abstract class BaseController implements IController {
-
+        const ACTION_SUFFIX = "Action";
+        const CONTROLLER_SUFFIX = "Controller";
+        /**
+         * @var Request
+         */
         protected $_request;
+        /**
+         * @var View
+         */
         protected $_view;
 
-        public function __construct( Request $request, View $view ) {
+        /**
+         * @var IDataProvider
+         */
+        protected $_data;
 
+        public function __construct( Request $request, View $view ) {
             $this->_request = $request;
             $this->_view    = $view;
-
+            $this->_data    = $this->_view->getDataProvider();
             $this->init();
         }
 
         protected function init() {}
 
+        /**
+         * @param $action
+         * @return bool
+         */
         public function hasAction( $action ) {
-            return (bool)method_exists( $this, $action . ACTION_SUFFIX );
+            return (bool)method_exists( $this, $action . self::ACTION_SUFFIX );
         }
 
         static public function getControllerName(){
             $class = get_called_class();
             $class = explode('\\',$class);
             $class = $class[count($class)-1];
-            $class = explode('Controller',$class);
+            $class = explode(self::CONTROLLER_SUFFIX,$class);
             return strtolower($class[0]);
         }
 
-        public function indexAction() {}
+        public function indexAction() {
+            $this->_view->render();
+        }
 
+        /**
+         * @return View
+         */
+        public function getView(){
+            return $this->_view;
+        }
+
+        public function toView($key, $value) {
+            $this->_data->$key = $value;
+        }
     }
 }
