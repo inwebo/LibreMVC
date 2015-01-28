6788c957c3ec40e51a7477b94ff05b0fa2f85f5d
inwebo <inwebo@gmail.com>
2015-01-28 22:09:17 +0100

    Test Hooks::__layout

--- modules/foo/autoload.php
+++ modules/foo/autoload.php
@@ -1,3 +1,14 @@
 <?php
 registerModule();
 use LibreMVC\Modules\Foo as Module;
+
+use LibreMVC\System;
+use LibreMVC\System\Hooks\CallBack;
+
+$hooks = System::this()->hooks;
+
+$hooks->__layout->attachCallback(new CallBack(function(){
+    $module = getModule('admin');
+    $path = $module->getPath()->getBaseDir('index');
+    return $path;
+}));
