7bb9e9ec5d7139c2b2281eb1e152fb153f4199cb
inwebo <inwebo@gmail.com>
2015-01-28 13:54:11 +0100

    Refactoring patterns

--- core/patterns/autoload.php
+++ core/patterns/autoload.php
@@ -1,2 +1,6 @@
 <?php
-include('isingleton/class.isingleton.php');
+include('adjustablepriorityqueue/class.adjustablepriorityqueue.php');
+include('multiton/class.multiton.php');
+include('observer/class.observer.php');
+include('observer/observable/class.observable.php');
+include('singleton/class.singleton.php');
--- core/patterns/multiton/class.multiton.php
+++ core/patterns/multiton/class.multiton.php
@@ -1,63 +1,28 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: inwebo
- * Date: 18/08/13
- * Time: 00:17
- * To change this template use File | Settings | File Templates.
- */
+namespace LibreMVC\System\Patterns {
+    class Multiton {
+        /**
+         * Doit être surchargé !
+         * @var \StdClass
+         */
+        static protected $_instances;
+        private function __construct() {}
+        private function __clone() {}
 
-namespace LibreMVC\System;
+        /**
+         * @param $name
+         * @return mixed
+         */
+        static public function get( $name ) {
+            if( is_null( static::$_instances ) ) {
+                static::$_instances = new \StdClass();
+            }
 
-class Multiton {
+            if( !isset( static::$_instances->$name ) ) {
+                static::$_instances->$name = new self();
+            }
 
-    public $readOnly = false;
-    protected  static $instances;
-
-    private function __construct() {}
-    private function __clone() {}
-
-    /**
-     * Retourne
-     *
-     * @param string $name
-     * @return Mixed
-     */
-    static public function get( $name ) {
-        $calledClass = get_called_class();
-
-        if( is_null( $calledClass::$instances ) ) {
-            $calledClass::$instances = new \StdClass();
-        }
-
-        if( !isset( $calledClass::$instances->$name ) ) {
-            $calledClass::$instances->$name = new $calledClass;
-        }
-
-        return $calledClass::$instances->$name;
-    }
-
-    static public function self() {
-        $calledClass = get_called_class();
-        return $calledClass::$instances;
-    }
-
-    /**
-     * Setter
-     */
-    public function __set($key, $value) {
-        if( !$this->readOnly ) {
-            $this->$key = $value;
+            return static::$_instances->$name;
         }
     }
-
-    /**
-     * Getter
-     */
-    public function __get($key) {
-        if (isset($this->$key)) {
-            return $this->$key;
-        }
-    }
-
 }
