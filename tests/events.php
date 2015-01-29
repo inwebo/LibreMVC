dc9839a4274f9fe32788e03ddd0ad7e3b2b577c0
inwebo <inwebo@gmail.com>
2015-01-29 17:05:52 +0100

    New boot system sandbox

---
+++ tests/events.php
@@ -0,0 +1,122 @@
+<?php
+
+include('../core/system/boot/autoload.php');
+include('../core/system/class.system.php');
+include('../core/files/autoload.php');
+include('../core/patterns/autoload.php');
+
+use LibreMVC\Patterns\Observer;
+use LibreMVC\Patterns\Observer\Observable;
+use LibreMVC\Files\Config;
+
+$ini = 'demo/config.ini';
+$config = Config::load($ini);
+
+interface IBootable{
+    public function start();
+    public function end();
+}
+
+class Tasks extends \SplObjectStorage{
+
+    protected $_name;
+
+    function __construct($_name) {
+        $this->_name = $_name;
+    }
+
+
+}
+
+class Events extends Observer{
+    public function update( \SplSubject $subject ) {
+        $subject->end();
+    }
+}
+
+class Task extends Observable implements IBootable{
+
+    public function start() {
+
+    }
+
+    public function end(){
+        echo "Task :" . __CLASS__ . " done";
+    }
+
+    protected function onEnd(){
+        $this->notify();
+    }
+}
+
+class Boot {
+    /**
+     * @var
+     */
+    protected $_config;
+    /**
+     * @var SplObserver
+     */
+    protected $_observer;
+    /**
+     * @var Tasks
+     */
+    protected $_tasks;
+    /**
+     * @var
+     */
+    protected $_dataProvider;
+
+    function __construct($_config, SplObserver $_observer, Tasks $_tasks, $_dataProvider) {
+        $this->_config          = $_config;
+        $this->_dataProvider    = $_dataProvider;
+        $this->_observer        = $_observer;
+        $this->_tasks           = $_tasks;
+    }
+
+    public function start(){
+        $this->_tasks->rewind();
+        while($this->_tasks->valid()) {
+            $task = $this->_tasks->current();
+            $class = get_class($task);
+            $reflectionClass = new ReflectionClass($class);
+            $methods = $reflectionClass->getMethods(ReflectionMethod::IS_PROTECTED);
+            $methods = new ArrayIterator($methods);
+
+            while($methods->valid()) {
+                $method = $methods->current();
+                try {
+                    $reflectionMethod = new ReflectionMethod($class,$method->getName());
+                    $reflectionMethod->setAccessible(true);
+                    $reflectionMethod->invoke($task);
+                }
+                catch(\Exception $e) {
+                    var_dump($e);
+                }
+                $methods->next();
+            }
+
+            $this->_tasks->next();
+        }
+    }
+
+    protected function isValidMethod(ReflectionMethod $reflectionMethod) {
+        return ($reflectionMethod->isProtected() && !$reflectionMethod->isStatic() );
+    }
+
+}
+
+$observer = new Events('BootStrap');
+$step = new Task();
+$step->attach($observer);
+$step2 = new Task();
+$step2->attach($observer);
+
+$tasks = new Tasks("Steps");
+$tasks->attach($step);
+//$tasks->attach($step2);
+
+$data = \LibreMVC\System::this();
+
+$boot = new Boot($config, $observer, $tasks, $data);
+$boot->start();
