896efe5e1ac45acb2975a8bcc4b1383cdfbe8faf
inwebo <inwebo@gmail.com>
2015-01-29 17:10:27 +0100

    New Boot system

--- core/system/boot/class.boot.php
+++ core/system/boot/class.boot.php
@@ -3,35 +3,77 @@
 namespace LibreMVC\System {
 
     use LibreMVC\System;
-    use LibreMVC\System\Boot\IBootable;
+    use LibreMVC\System\Boot\IStepable;
 
-    class Boot {
-
-        protected $_steps;
+    class Boot
+    {
+        /**
+         * @var
+         */
+        protected $_config;
+        /**
+         * @var SplObserver
+         */
+        protected $_observer;
+        /**
+         * @var Tasks
+         */
+        protected $_tasks;
+        /**
+         * @var
+         */
         protected $_dataProvider;
 
-        public function __construct( IBootable $steps, $dataProvider ) {
-            if( is_object( $steps ) ) {
-                $this->_steps = $steps;
-                $this->_dataProvider = $dataProvider;
-                $this->start();
-            }
-            else {
-                throw new \Exception( __CLASS__ . ' must have valid callback object');
+        public function __construct(\SplObserver $_observer, IStepable $_tasks, $_dataProvider)
+        {
+            $this->_dataProvider = $_dataProvider;
+            $this->_observer = $_observer;
+            $this->_tasks = $_tasks;
+            $this->attachObserver();
+        }
+
+        public function attachObserver(){
+            $this->_tasks->rewind();
+            while ($this->_tasks->valid()) {
+                $task = $this->_tasks->current();
+                $task->attach($this->_observer);
+                $this->_tasks->next();
             }
         }
 
-        public function start() {
-            $members = get_class_methods( $this->_steps );
-            foreach( $members as  $member ) {
-                $reflectAssertions = new \ReflectionMethod( $this->_steps, $member );
-                if($reflectAssertions->isStatic() && $reflectAssertions->isPublic()) {
-                    $val = $reflectAssertions->invoke($member);
-                    if( !is_null($val) ) {
-                        $this->_dataProvider->$member = $val;
+        public function start()
+        {
+            $this->_tasks->rewind();
+            while ($this->_tasks->valid()) {
+                $task = $this->_tasks->current();
+                $class = get_class($task);
+                $reflectionClass = new \ReflectionClass($class);
+                $methods = $reflectionClass->getMethods(\ReflectionMethod::IS_PROTECTED);
+                $methods = new \ArrayIterator($methods);
+
+                while ($methods->valid()) {
+                    $method = $methods->current();
+                    try {
+                        $reflectionMethod = new \ReflectionMethod($class, $method->getName());
+                        $reflectionMethod->setAccessible(true);
+                        $result = $reflectionMethod->invoke($task);
+                        $name = $method->getName();
+                        if(!is_null($result)) {
+                            $this->_dataProvider->this()->$name = $result;
+                        }
+                    } catch (\Exception $e) {
+                        var_dump($e);
                     }
+                    $methods->next();
                 }
+
+                $this->_tasks->next();
             }
+        }
+
+        protected function isValidMethod(ReflectionMethod $reflectionMethod)
+        {
+            return ($reflectionMethod->isProtected() && !$reflectionMethod->isStatic());
         }
 
     }
