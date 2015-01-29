09a50865473efecb92f3cdbb156256a9c2388d1a
inwebo <inwebo@gmail.com>
2015-01-29 17:11:21 +0100

    Del Hooks

--- modules/foo/autoload.php
+++ modules/foo/autoload.php
@@ -2,13 +2,3 @@
 registerModule();
 use LibreMVC\Modules\Foo as Module;
 
-use LibreMVC\System;
-use LibreMVC\System\Hooks\CallBack;
-
-$hooks = System::this()->hooks;
-
-$hooks->__layout->attachCallback(new CallBack(function(){
-    $module = getModule('admin');
-    $path = $module->getPath()->getBaseDir('index');
-    return $path;
-}));
