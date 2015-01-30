a88f6e45a9b7f6e112d8cc025f2cd00fb5f244e2
inwebo <inwebo@gmail.com>
2015-01-31 00:22:50 +0100

    Update doc

--- core/database/autoload.php
+++ core/database/autoload.php
@@ -1,8 +1,13 @@
 <?php
-include('driver/basedriver/class.basedriver.php');
 include('driver/idriver/class.idriver.php');
+include('driver/basedriver/class.basedriver.php');
 include('driver/class.driver.php');
 include('driver/mysql/class.mysql.php');
 include('driver/sqlite/class.sqlite.php');
-include('provider/class.provider.php');
-include('statement/class.statement.php');
+
+include('drivers/class.drivers.php');
+
+include('entity/autoload.php');
+include('query/class.query.php');
+include('results/class.results.php');
+
