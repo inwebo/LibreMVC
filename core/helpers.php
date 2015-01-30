30b7b2663735bbbb27620e1440c3fdbc8b294baf
inwebo <inwebo@gmail.com>
2015-01-31 00:23:22 +0100

    Update

--- core/helpers.php
+++ core/helpers.php
@@ -3,14 +3,14 @@
 use LibreMVC\Autoloader\Handler;
 use LibreMVC\Autoloader\Decorators;
 
-function addRoute($pattern, $controller, $action){
+function addRoute($pattern, $controller, $action, $collection="default"){
     $uri = \LibreMVC\System::this()->instance->getBaseUri();
     $route = new \LibreMVC\Routing\Route(
         $uri .= $pattern,
         $controller,
         $action
     );
-    \LibreMVC\Routing\RoutesCollection::get('default')->addRoute($route);
+    \LibreMVC\Routing\RoutesCollection::get($collection)->addRoute($route);
 }
 function addStaticRoute($pattern){
     $uri = \LibreMVC\System::this()->instance->getBaseUri();
@@ -28,6 +28,10 @@ function registerModule(){
 function registerInstance(){
     $path = \LibreMVC\System::this()->instance->getRealPath();
     Handler::addDecorator(new Decorators($path));
+}
+
+function getBaseUrl(){
+    return \LibreMVC\System::this()->basePaths->getBaseUrl();
 }
 
 function getModules(){
