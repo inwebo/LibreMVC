1ed47a4ab5432cfc2cdb3ad28b1db1e2e99e3c71
inwebo <inwebo@gmail.com>
2015-01-31 00:25:18 +0100

    Update

--- modules/admin/autoload.php
+++ modules/admin/autoload.php
@@ -1,27 +1,4 @@
 <?php
 registerModule();
 use LibreMVC\Modules\Admin as Module;
-addRoute("admin/routes/[:action]",'\\LibreMVC\\Modules\\Admin\\Controllers\\RoutesController','index');
-/*
-use LibreMVC\System;
-use LibreMVC\System\Hooks\CallBack;
-
-$hooks  = System::this()->hooks;
-$routed = System::this()->routed;
-*/
-//var_dump($routed);
-
-/* Systematique
-$hooks->__layout->attachCallback(new CallBack(function(){
-    $module = getModule('admin');
-    $path = $module->getPath()->getBaseDir('index');
-    return $path;
-}));
-*/
-/*
-$hooks->__layout_body_partial->attachCallback(new CallBack(function(){
-    $module = getModule('admin');
-    $path = $module->getPath()->getBaseDir('views');
-    return $path;
-}));
-*/
+addRoute("admin[/][:action]",'\\LibreMVC\\Modules\\Admin\\Controllers\\HomeController','index');
