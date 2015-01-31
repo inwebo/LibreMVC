c1b8be6d67b37131b87395fe3b400e6fd5078f9b
inwebo <inwebo@gmail.com>
2015-02-01 00:35:41 +0100

    Update @todo _statement

--- core/database/entity/class.entity.php
+++ core/database/entity/class.entity.php
@@ -9,7 +9,16 @@ namespace LibreMVC\Database {
     abstract class Entity implements IModelable
     {
 
-        const SQL_SELECT = "";
+        const SQL_LOAD ='Select * from `%s` WHERE %s=? LIMIT 1';
+        const SQL_DELETE ='Delete from `%s` WHERE %s=? LIMIT 1';
+        const SQL_DELETE_MULTIPLE ='Delete * from `%s` WHERE %s=? IN %s LIMIT 1';
+        const SQL_INSERT ='INSERT INTO %s %s VALUES %s';
+        const SQL_UPDATE ='UPDATE %s SET %s WHERE %s=?';
+
+        /**
+         * @var string
+         */
+        protected $_loaded;
 
         /**
          * @var EntityConfiguration
@@ -20,45 +29,91 @@ namespace LibreMVC\Database {
             $this->init();
         }
 
-        protected function init() {}
+        protected function init() {
+            $this->_loaded = false;
+        }
 
-        static public function binder(IDriver $iDriver, $pk = null, $table = null, $tableDesc = null){
-            $i = $iDriver;
-            $t = (!is_null($table)) ? $table : self::classToTable() . 's';
-            $p = (!is_null($pk)) ? $pk : $i->getPrimaryKey($t);
-            $td =$i->getColsName($t);
-            $conf = new EntityConfiguration($i, $p, $t, $td);
+        /**
+         * @param bool $statement
+         */
+        protected function setLoaded($statement) {
+            $this->_loaded = $statement;
+        }
+
+        static public function binder(IDriver $iDriver, $primaryKey = null, $tableName = null, $tableDesc = null){
+            $_table = (!is_null($tableName)) ? $tableName : self::getShortName() . 's';
+            $pk     = (!is_null($primaryKey)) ? $primaryKey : $iDriver->getPrimaryKey($_table);
+            $cols   = $iDriver->getColsName($_table);
+            $conf   = new EntityConfiguration($iDriver, $pk, $_table, $cols);
             static::$_entityConfiguration = $conf;
+        }
+
+        public function save(){
+            $conf = static::$_entityConfiguration;
+            $toBind = $this->getValues();
+            $toBindKeys = array_keys($toBind);
+            $toBindValues = array_values($toBind);
+            $sqlKeys = $conf->aggregateCols($toBindKeys);
+            $tokens = $conf->aggregateCols($conf->getTokens(count($toBindKeys)));
+            if($this->_loaded) {
+                // Update
+                $sqlUpdateQuery = sprintf(self::SQL_UPDATE, $conf->table,$conf->toUpdate($toBindKeys),$conf->primaryKey);
+                $toInject = array_merge($toBindValues, array($this->id));
+                $conf->driver->query($sqlUpdateQuery,$toInject);
+                //var_dump($sqlKeys,$sqlValues,$sqlUpdateQuery);
+                //var_dump($conf->toColsName($toBindKeys));
+                //var_dump($toInject);
+            }
+            else {
+                // Insert
+                $sqlInsertQuery = sprintf(self::SQL_INSERT, $conf->table, $sqlKeys, $tokens);
+                //$conf->driver->query($sqlInsertQuery, $toBindValues);
+            }
+        }
+
+        public function delete() {
+            $sqlDelete = sprintf(self::SQL_DELETE, $this->getConf()->table, $this->getConf()->primaryKey);
+            $this->getConf()->driver->query($sqlDelete,array($this->id));
+        }
+
+        static public function deleteByIds($array){
+            $conf = static::$_entityConfiguration;
+            $sqlDelete =sprintf(self::SQL_DELETE_MULTIPLE, $conf->table,$conf->primaryKey,$conf->aggregateCols($array));
         }
 
         static public function load($id) {
             $conf = static::$_entityConfiguration;
             // Est-il configuré ?
             if (!is_null($conf)) {
-                $conf->driver->toObject(self::getCalledClass());
-                return $conf->driver->query('select * from ' . $conf->table . ' WHERE ' . $conf->primaryKey . "=? LIMIT 1",array($id))->first();
+                $conf->driver->toObject(get_called_class());
+                $sqlSelect =  sprintf(self::SQL_LOAD,$conf->table,$conf->primaryKey);
+                $obj = $conf->driver->query($sqlSelect,array($id))->first();
+                if( !is_null($obj) ) {
+                    $obj->setLoaded(true);
+                    return $obj;
+                }
             } else {
                 throw new Exception("Bind model first");
             }
         }
 
-        static public function classToTable() {
-            $class = self::getCalledClass();
-            $table = array();
-            // Is namespaced
-            if ((strpos($class, '\\') !== false)) {
-                $table = explode('\\', trim($class, '\\'));
-                $table = end($table);
-            } // nope
-            else {
-                $table = $class;
-            }
-            return $table;
+        static public function getConf() {
+            return static::$_entityConfiguration;
         }
 
-        static public function getCalledClass()
-        {
-            return get_called_class();
+        static public function getShortName() {
+            $ref = new \ReflectionClass( get_called_class() );
+            return $ref->getShortName();
         }
+
+        protected function getValues() {
+            $members = $this->getConf()->intersectObj($this);
+            $array = array();
+            foreach($members as $v) {
+                $array[$v] = $this->$v;
+            }
+            return $array;
+        }
+
     }
 }
