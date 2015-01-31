33c7a46ae57dba1728734f1fa5550e53ed780f13
inwebo <inwebo@gmail.com>
2015-02-01 00:37:13 +0100

    Add modulesAction()

--- modules/admin/controllers/homecontroller/class.homecontroller.php
+++ modules/admin/controllers/homecontroller/class.homecontroller.php
@@ -10,5 +10,19 @@ namespace LibreMVC\Modules\Admin\Controllers {
             $this->toView("routes", RoutesCollection::get("default"));
             $this->render();
         }
+
+        public function modulesAction() {
+            $modules = $this->_system->appPaths->getBaseDir('modules');
+            $availableModules = glob($modules . '*');
+            $buffer = array();
+            foreach($availableModules as $k => $v) {
+                $buffer[] = basename($v);
+            }
+            $this->toView('available', $buffer);
+
+            $enabledModules = $this->_system->getModules();
+            $this->toView('enabled', $enabledModules);
+            $this->render();
+        }
     }
 }
