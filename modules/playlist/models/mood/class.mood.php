e6b003edf6ebbf0adb4da3b446f3c4eecf968e07
inwebo <inwebo@gmail.com>
2015-02-01 00:37:22 +0100

    Cleaned

--- modules/playlist/models/mood/class.mood.php
+++ modules/playlist/models/mood/class.mood.php
@@ -1,22 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inwebo
- * Date: 25/01/15
- * Time: 23:53
- */
 
 namespace LibreMVC\Modules\Playlist\Models;
 
-
 use LibreMVC\Database\Entity;
-use LibreMVC\Database\Temp\_Entity;
 
 class Mood extends Entity{
-    static public $_primaryKey;
-    static public $_table;
-    static public $_tableDescription;
-    static public $_statement;
+
     protected $id;
     public $name;
 
