cad99c791bf0ec4579235c1289e6ae9652233d6b
inwebo <inwebo@gmail.com>
2015-02-02 00:15:28 +0100

    Add User helper

--- core/helpers.php
+++ core/helpers.php
@@ -2,7 +2,17 @@
 
 use LibreMVC\Autoloader\Handler;
 use LibreMVC\Autoloader\Decorators;
-
+function user() {
+    return (isset($_SESSION['User'])) ? $_SESSION['User'] : null;
+}
+function isDefaultUser(){
+    if(isset($_SESSION['User'])) {
+        return ( user()->isDefault() );
+    }
+    else {
+        return true;
+    }
+}
 function addRoute($pattern, $controller, $action, $collection="default"){
     $uri = \LibreMVC\System::this()->instance->getBaseUri();
     $route = new \LibreMVC\Routing\Route(
@@ -21,7 +31,6 @@ function addStaticRoute($pattern){
     );
     \LibreMVC\Routing\RoutesCollection::get('default')->addRoute($route);
 }
-
 function registerModule(){
     Handler::addDecorator(new Decorators(getcwd()));
 }
@@ -29,11 +38,9 @@ function registerInstance(){
     $path = \LibreMVC\System::this()->instance->getRealPath();
     Handler::addDecorator(new Decorators($path));
 }
-
 function getBaseUrl(){
     return \LibreMVC\System::this()->basePaths->getBaseUrl();
 }
-
 function getModules(){
     return \LibreMVC\System::this()->modules;
 }
