931070d9953dc291859e82de013d4ed619a89e2b
inwebo <inwebo@gmail.com>
2015-02-02 00:16:08 +0100

    Mover reflection

--- core/database/driver/class.driver.php
+++ core/database/driver/class.driver.php
@@ -8,6 +8,12 @@ namespace LibreMVC\Database {
 
         public function query($query, $params = array() ) {
             $pdoStatement = $this->driver->prepare($query);
+            if( isset($this->toObject) ) {
+                // Init constructor params.
+                $reflection = new \ReflectionMethod($this->toObject,'__construct');
+                $parameters = $reflection->getParameters();
+                $pdoStatement->setFetchMode(\PDO::FETCH_CLASS, $this->toObject,$parameters);
+            }
             try {
                 (!is_null($params) && is_array($params)) ?
                     $pdoStatement->execute($params) :
@@ -15,12 +21,6 @@ namespace LibreMVC\Database {
             }
             catch(\Exception $e) {
                 var_dump($e);
-            }
-            if( isset($this->toObject) ) {
-                // Init constructor params.
-                $reflection = new \ReflectionMethod($this->toObject,'__construct');
-                $parameters = $reflection->getParameters();
-                $pdoStatement->setFetchMode(\PDO::FETCH_CLASS, $this->toObject,$parameters);
             }
             return new Results($pdoStatement);
         }
