2bf7b0517bccf761ba229901fdbd612297cb5a1a
inwebo <inwebo@gmail.com>
2015-02-02 10:42:17 +0100

    Fix namesapce

--- core/models/user/class.user.php
+++ core/models/user/class.user.php
@@ -2,7 +2,9 @@
 
 namespace LibreMVC\Models {
     use LibreMVC\Database\Entity;
-    class User extends Entity{
+    use LibreMVC\Models\User\IAuth;
+
+    class User extends Entity implements IAuth{
 
         /**
          * @var int
@@ -33,14 +35,15 @@ namespace LibreMVC\Models {
          */
         public $privateKey;
 
-        function __construct($login, $mail, $password, $passPhrase) {
-            parent::__construct();
-            $this->login = $login;
-            $this->mail = $mail;
-            $this->password = sha1($password);
-            $this->passPhrase = sha1($passPhrase);
-            $this->publicKey = $this->hashPublicKey();
-            $this->privateKey = self::hashPrivateKey($this->login, $this->publicKey, $this->passPhrase);
+        static public function build($login, $mail, $password, $passPhrase) {
+            $obj = new static;
+            $obj->login = $login;
+            $obj->mail = $mail;
+            $obj->password = sha1($password);
+            $obj->passPhrase = sha1($passPhrase);
+            $obj->publicKey = $obj->hashPublicKey();
+            $obj->privateKey = self::hashPrivateKey($obj->login, $obj->publicKey, $obj->passPhrase);
+            return $obj;
         }
 
         protected function hashPublicKey() {
@@ -49,6 +52,32 @@ namespace LibreMVC\Models {
 
         static public function hashPrivateKey( $user, $publicKey, $passPhrase ) {
             return base64_encode( hash_hmac( "sha256", $user , $publicKey . $passPhrase ) ) ;
+        }
+        static public function validatePublicKey(User $user){
+
+        }
+        public function hasPermission($id) {
+            return false;
+        }
+
+        public function hasRole($id) {
+            return false;
+        }
+
+        public function isDefault() {
+            return (is_null($this->id));
+        }
+
+        public function hidden() {
+            $clone = clone $this;
+            unset($clone->privateKey);
+            unset($clone->password);
+            unset($clone->passPhrase);
+            return $clone;
+        }
+
+        static public function loadByIdPwd( $id, $pwd ) {
+            return null;
         }
 
     }
