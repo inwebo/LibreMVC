5306988dd27de897045f9a14b66010b3f7ff61e9
inwebo <inwebo@gmail.com>
2015-02-02 10:42:06 +0100

    Update

--- core/models/module/class.module.php
+++ core/models/module/class.module.php
@@ -1,35 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inwebo
- * Date: 28/01/15
- * Time: 20:41
- */
 
-namespace LibreMVC\Models;
+namespace LibreMVC\Models {
 
+    use LibreMVC\Web\Instance\PathsFactory\Path;
 
-use LibreMVC\Web\Instance\PathsFactory\Path;
+    class Module {
 
-class Module {
+        protected $_name;
+        /**
+         * @var Path
+         */
+        protected $_path;
 
-    protected $_name;
-    /**
-     * @var Path
-     */
-    protected $_path;
-
-    public function __construct($name, Path $path) {
-        $this->_name = strtolower($name);
-        $this->_path = $path;
+        public function __construct($name, Path $path) {
+            $this->_name = strtolower($name);
+            $this->_path = $path;
+        }
+        public function getPath(){
+            return $this->_path;
+        }
+        public function getName() {
+            return $this->_name;
+        }
+        public function getBaseDir(){
+            return $this->_path->dir();
+        }
+        public function getDirViews() {
+            return $this->_path->getBaseDir("views");
+        }
+        public function getIndex() {
+            return $this->_path->getBaseDir("index");
+        }
+        public function getStaticDir() {
+            return $this->_path->getBaseDir("static_views");
+        }
     }
-
-    public function getPath(){
-        return $this->_path;
-    }
-
-    public function getName() {
-        return $this->_name;
-    }
-
 }
