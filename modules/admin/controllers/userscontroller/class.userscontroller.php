aee2029031e8d383cd6d818230de2e21080358d2
inwebo <inwebo@gmail.com>
2015-02-02 21:39:50 +0100

    Add users list

--- modules/admin/controllers/userscontroller/class.userscontroller.php
+++ modules/admin/controllers/userscontroller/class.userscontroller.php
@@ -5,9 +5,17 @@ namespace LibreMVC\Modules\Admin\Controllers {
     use LibreMVC\System;
     use LibreMVC\Routing\RoutesCollection;
 
-    class UserController extends AdminController {
+    class UsersController extends AdminController {
         public function indexAction() {
             $driver = AuthUser::getBoundDriver();
+            $driver->toStdClass();
+            $user = $driver->query("select id FROM Users")->all();
+            $users = new \ArrayIterator();
+            foreach($user as $u) {
+                $users->append(AuthUser::load($u->id));
+            }
+            $users->rewind();
+            $this->toView("users", $users);
             $this->render();
         }
     }
