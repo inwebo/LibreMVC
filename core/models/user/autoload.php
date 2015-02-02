9252680c34b49644461f3ac381bddd671a5ec58a
inwebo <inwebo@gmail.com>
2015-02-02 21:31:43 +0100

    Add interface iauth

--- core/models/user/autoload.php
+++ core/models/user/autoload.php
@@ -1,4 +1,5 @@
 <?php
+include('iauth/class.iauth.php');
 include('role/filters/permissionfilter/class.permissionfilter.php');
 include('role/filters/rolefilter/class.rolefilter.php');
 include('role/permission/class.permission.php');
