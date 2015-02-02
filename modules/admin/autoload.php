a050f6849f46f871d2c7ff29348307ff51fa04e3
inwebo <inwebo@gmail.com>
2015-02-02 10:42:27 +0100

    Add users route

--- modules/admin/autoload.php
+++ modules/admin/autoload.php
@@ -1,4 +1,4 @@
 <?php
 registerModule();
-use LibreMVC\Modules\Admin as Module;
 addRoute("admin[/][:action]",'\\LibreMVC\\Modules\\Admin\\Controllers\\HomeController','index');
+addRoute("admin/users/[:action]",'\\LibreMVC\\Modules\\Admin\\Controllers\\UsersController','index');
