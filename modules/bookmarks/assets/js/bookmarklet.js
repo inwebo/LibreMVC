3c96e36946fa942062062925f78a2f8db28ff903
inwebo <inwebo@gmail.com>
2015-02-02 21:43:34 +0100

    Introduce bookmarklet

---
+++ modules/bookmarks/assets/js/bookmarklet.js
@@ -0,0 +1,32 @@
+javascript:(function(){
+
+    var user, publicKey, restService, bookmark, xpathFavicon, metas;
+
+    user            = '<?php echo $this->user ?>';
+    publicKey       = '<?php echo $this->publicKey ?>';
+    restService     = '<?php echo $this->restService ?>';
+    xpathFavicon    = document.evaluate('//*[contains(@rel,\'shortcut icon\')]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+    bookmark        = {
+        url:null,
+        title:null,
+        favicon:null
+    } ;
+    bookmark.url    = encodeURIComponent(location.href);
+    bookmark.title  = encodeURIComponent(document.title);
+    bookmark.favicon= (xpathFavicon.snapshotLength != 0) ? encodeURIComponent(xpathFavicon.snapshotItem(0).getAttribute('href'))  : null;
+
+    console.log(bookmark);
+
+    metas = document.getElementsByTagName('meta');
+    var y = metas.length;
+    for (var x=0 ; x<y; x++) {
+        if (metas[x].name.toLowerCase() == 'description') {
+            bookmark.description = encodeURIComponent(metas[x].content);
+        }
+        if (metas[x].name.toLowerCase() == 'keywords') {
+            bookmark.keywords = encodeURIComponent(metas[x].content);
+        }
+    }
+    window.open(restService+'?user='+user+'&publicKey='+publicKey+'&url='+bookmark.url+'&title='+bookmark.title+'&description='+'&keywords='+bookmark.keywords+'&favicon='+bookmark.favicon,'Save me !','location=0,titlebar=0,toolbar=0,menubar=0,resizable=0,width=300,height=550,left=0,top=0').focus();
+})();
---
+++ modules/bookmarks/views/static/bookmarklet.php
@@ -0,0 +1,13 @@
+    <div class="col-md-12">
+        <div class="col-container">
+            <h3>Widget</h3>
+            <div id="displayWidget" class="alert alert-info text-center">
+                <p>
+                    Please drag and drop to your toolbar.<br>
+                    &#8595;<br>
+                    <a id="#widget-template" href="<?php $this->partial( 'bookmarklet' )->render(); ?>" class="btn btn-default col-sm-12 alert alert-success">&hearts;</a>
+                </p>
+
+            </div>
+        </div>
+    </div>
