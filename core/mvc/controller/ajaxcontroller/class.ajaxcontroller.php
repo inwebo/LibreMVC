7a96d72bfe264010352bcee3bc4bd26e7472f8d3
inwebo <inwebo@gmail.com>
2015-02-02 21:43:19 +0100

    Add ajax controller

---
+++ core/mvc/controller/ajaxcontroller/class.ajaxcontroller.php
@@ -0,0 +1,89 @@
+<?php
+namespace LibreMVC\Mvc\Controller;
+use LibreMVC\Http\Header;
+use LibreMVC\Models\AjaxResponse;
+use LibreMVC\View;
+
+class AjaxController extends BaseController{
+
+    /**
+     * @var bool Le resultat de la requête doit il être mis en cache ?
+     */
+    protected $_cachable = false;
+    /**
+     * @var AjaxResponse L'objet de réponse
+     */
+    protected $_reply;
+    /**
+     * @var string To set
+     * @todo TO SET to prod
+     */
+    protected $_salt = "salt";
+
+    public $_inputs;
+
+    public function init() {
+        $this->_reply = new AjaxResponse();
+        // Réponse est elle cachable ?
+        if( !$this->_cachable ) {
+            Header::noCache();
+        }
+        $this->_view->setEmptyLayout();
+        $this->negotiateHttpContentType();
+        // Peuple.
+        $this->initInputs();
+    }
+
+    protected function initInputs() {
+        if( isset($_GET) ) {
+            $this->_inputs = $_GET;
+        }
+        else {
+            parse_str(file_get_contents('php://input'), $this->_inputs);
+        }
+
+
+    }
+
+    protected function negotiateHttpContentType() {
+        switch($this->getRequest()->getHeader('Accept')) {
+            case 'application/json':
+                Header::json();
+                break;
+
+            case 'text/xml':
+                Header::xml();
+                break;
+            default:
+            case 'text/html':
+                Header::html();
+                break;
+
+            case 'text/plain':
+                Header::textPlain();
+                break;
+        }
+    }
+
+    public function __destruct() {
+        switch($this->getRequest()->getHeader('Accept')) {
+
+            case 'application/json':
+                echo $this->_reply->toJson();
+                break;
+
+            case 'text/xml':
+                echo $this->_reply->toXml();
+                break;
+
+            case 'text/html':
+                echo $this->_reply->toHtml();
+                break;
+
+            default:
+            case 'text/plain':
+                echo $this->_reply->toString();
+                break;
+        }
+    }
+}
---
+++ modules/bookmarks/controllers/ajaxbookmarkcontroller/class.ajaxbookmarkcontroller.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inwebo
+ * Date: 24/01/15
+ * Time: 20:08
+ */
+
+namespace LibreMVC\Modules\Bookmarks\Controllers;
+
+
+use LibreMVC\Files\Config;
+use LibreMVC\Helpers\Pagination;
+use LibreMVC\Modules\Bookmarks\Models\Tags;
+use LibreMVC\Mvc\Controller\AjaxController;
+use LibreMVC\Mvc\Controller\BaseController;
+use LibreMVC\System;
+use LibreMVC\Database\Drivers;
+use LibreMVC\Database\Driver\MySql;
+use LibreMVC\View;
+
+class AjaxBookmarkController extends AjaxController{
+
+    public function indexAction(){
+
+    }
+
+}
