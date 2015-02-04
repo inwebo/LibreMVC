dc588e9acd3b71607d643677d3eb4fde1b424d7c
inwebo <inwebo@gmail.com>
2015-02-04 19:35:55 +0100

    Add helpers

--- core/mvc/controller/basecontroller/class.basecontroller.php
+++ core/mvc/controller/basecontroller/class.basecontroller.php
@@ -32,7 +32,7 @@ namespace LibreMVC\Mvc\Controller {
 
         public function __construct( Request $request, System $system) {
             $this->_request = $request;
-            $this->_system = $system;
+            $this->_system  = $system;
             $this->_view    = $this->_system->layout;
             $this->_vo      = $this->_view->getViewObject();
             $this->init();
@@ -103,10 +103,29 @@ namespace LibreMVC\Mvc\Controller {
         }
 
         public function preRender(){}
+
+        /**
+         * @todo, selon un param $path
+         */
         public function render(){
             $this->_view->render();
         }
         public function postRender(){}
+        /**
+         * {controller}/{action}.php
+         * @param string $format
+         * @return string
+         */
+        protected function getViewPathFormat($format = "{controller}/{action}.php") {
+            $controller = static::getControllerName() ;
+            $action     = $this->_system->this()->routed->action;
+            $path       = str_replace(array('{controller}','{action}'),array($controller,$action),$format);
+            return $path;
+        }
+
+        protected function getViewFileFormat($format='{action}.{extension}',$extension = "php") {
+            return str_replace(array('{action}','{extension}'),array($this->_system->this()->routed->action,$extension),$format);
+        }
 
     }
 }
