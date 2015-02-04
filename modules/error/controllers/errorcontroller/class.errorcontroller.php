c6011df97c2a8e9c2855d86e8e13cbfd7f1f4a53
inwebo <inwebo@gmail.com>
2015-02-04 19:42:51 +0100

    Module local view

--- modules/error/controllers/errorcontroller/class.errorcontroller.php
+++ modules/error/controllers/errorcontroller/class.errorcontroller.php
@@ -1,27 +1,21 @@
 <?php
 
-namespace LibreMVC\Modules\Error\Controllers;
+namespace LibreMVC\Modules\Error\Controllers {
 
-use LibreMVC\Mvc\Controller\StaticController;
-use LibreMVC\View;
-class ErrorController extends StaticController{
+    use LibreMVC\Mvc\Controller\StaticController;
 
-    const GET = '\\LibreMVC\\Modules\\Error\\Controllers\\ErrorController';
+    class ErrorController extends StaticController{
 
-    public function init(){
-        parent::init();
-    }
+        const GET = '\\LibreMVC\\Modules\\Error\\Controllers\\ErrorController';
 
-    public function __call( $name, $arguments ) {
-        $this->_staticView  = $this->_staticView . $name.'.php';
-        $viewObject = $this->_view->getViewObject();
-        try{
-            //$viewObject->attachPartial('body', View::partialsFactory( $this->_staticView, $viewObject ));
-            $this->_view->attachPartial('body',$this->_staticView);
-            $this->_view->render();
+        public function init(){
+            parent::init();
+            $viewsPath = $this->_system->this()->getModuleBaseDirs('error','static_views') . $this->getViewFileFormat();
+            $this->changePartial('body',$viewsPath);
         }
-        catch(\Exception $e) {
-            var_dump($e);
+
+        public function __call( $name, $arguments ) {
+            $this->render();
         }
     }
 }
