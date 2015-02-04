b66ac0b97aad0c1578c9b1e9a99d18af7c7a7182
inwebo <inwebo@gmail.com>
2015-02-04 19:42:13 +0100

    Fix

--- modules/bookmarks/autoload.php
+++ modules/bookmarks/autoload.php
@@ -4,6 +4,10 @@ addRoute('bookmarks/[:action]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\Bo
 addRoute('bookmarks/page/[:id|page]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController','index');
 addRoute('bookmarks/tag/[:id|tag]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController','tag');
 addRoute('bookmarks/tags', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController','tags');
-addRoute('bookmarks/api/public', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\AjaxBookmarkController','index');
+addRoute('bookmarks/api/public[/][:action]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\AjaxBookmarkController','index');
 addRoute('bookmarks/api/private', '\\LibreMVC\\MVC\\Controller\\AjaxController\\PrivateAjaxController','index');
 addRoute('bookmarks/api/rest', '\\LibreMVC\\MVC\\Controller\\AjaxController\\PrivateAjaxController\\RestController','index');
+
+// Une route pour le formulaire, ajax privé
+
+// Une route pour ajouter un bookmark, rest privé
