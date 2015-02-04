82e10f94f34eab7d1984373aead8fdc718de1f6a
inwebo <inwebo@gmail.com>
2015-02-04 19:41:06 +0100

    Introduce module config

--- modules/authuser/autoload.php
+++ modules/authuser/autoload.php
@@ -1,6 +1,14 @@
 <?php
 use LibreMVC\Database\Driver\MySql;
 use LibreMVC\Modules\AuthUser\Models\AuthUser;
+use LibreMVC\Files\Config;
 registerModule();
-$driver = new MySql('localhost','UsersRoles','root','root');
+$file = getModule('authuser')->getPath()->getBaseDir('config');
+$config = Config::load($file);
+$driver = new MySql(
+    $config->Database["server"],
+    $config->Database["database"],
+    $config->Database["user"],
+    $config->Database["password"]
+);
 AuthUser::binder($driver,'id','Users');
