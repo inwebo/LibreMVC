3033c346ac9d641c421be359b66e3ff5de2bb65f
inwebo <inwebo@gmail.com>
2015-02-05 22:37:09 +0100

    @todo

---
+++ modules/bookmarks/controllers/formcontroller/class.formcontroller.php
@@ -0,0 +1,12 @@
+<?php
+namespace LibreMVC\Modules\Bookmarks\Controllers;
+
+use LibreMVC\Mvc\Controller\AjaxController\PrivateAjaxController;
+
+class FormController extends PrivateAjaxController{
+
+    public function indexAction() {
+
+    }
+
+}
