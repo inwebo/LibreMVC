2f823a6027dcfe22b59b5590cf073ce2305506dc
inwebo <inwebo@gmail.com>
2015-02-05 23:04:29 +0100

    @todo

--- tests/files.php
+++ tests/files.php
@@ -1,60 +1,191 @@
 <?php
 ini_set('display_errors', 'on');
 include('../core/files/autoload.php');
-include('../core/web/instance/autoload.php');
-include('../core/web/instance/pathsfactory/autoload.php');
 
-use LibreMVC\Files\Config;
+$demoFolder = getcwd()."/demo/io/";
+$writable = $demoFolder . "writable/";
+$locked = $demoFolder . "locked/";
+trait Modifiable {
 
-use LibreMVC\Web\Instance\PathsFactory;
+    public function isWritable( $filename ) {
+        if( is_file( $filename ) && is_writable( dirname( $filename ) ) ) {
+            $f = fopen( $filename , "r+" );
+            $result = ( is_writable( $filename ) && $f );
+            fclose($f);
+            return $result;
+        }
+        if( is_dir( $filename ) ) {
+            return is_writable($filename);
+        }
+        if( is_dir( dirname( $filename ) ) ) {
+            return is_writable( dirname( $filename ) );
+        }
+    }
 
-$ini = 'demo/config.ini';
-$ini2 = 'demo/config2.ini';
-$config = Config::load($ini);
-$config2 = Config::load($ini2);
+    public function write( $filename, $content ) {
+        if( $this->isWritable( $filename ) ) {
+            $handle = fopen( $filename, 'w+' );
+            if ( flock($handle, \LOCK_EX ) ) {
+                fwrite( $handle, $content );
+                flock( $handle, \LOCK_UN );
+                fclose( $handle );
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    public function append( $filename, $content ) {
+        if( $this->isWritable( $filename ) ) {
+            $handle = fopen( $filename, 'a' );
+            if ( flock($handle, \LOCK_EX ) ) {
+                fwrite( $handle, $content );
+                flock( $handle, \LOCK_UN );
+                fclose( $handle );
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    public function unlink( $filename ) {
+        if( $this->isWritable( $filename ) ) {
+            return unlink($filename);
+        }
+    }
+
+}
+
+trait isDistantFile {
+
+    public function isDistantFile( $filename ) {
+
+        if( filter_var( $filename, FILTER_VALIDATE_URL ) ) {
+
+            $opts = array(
+                'http' => array(
+                    'method' => 'HEAD'
+                )
+            );
+
+            $context = stream_context_create( $opts );
+
+            $file = file_get_contents( $filename, false, $context, null, 0 );
+
+            return ( $file !== false ) ? true : false;
+        }
+        return false;
+    }
+
+}
+
+class _Directory {
+
+    protected $_baseDir;
+
+    protected $_inodes;
+
+    public function __construct( $path, $isRecursive = true ) {
+        $this->_baseDir = $path;
+        try {
+            $recursiveDirectoryIterator = new \RecursiveDirectoryIterator(
+                $this->_baseDir,
+                \FilesystemIterator::SKIP_DOTS
+            );
+            $recursiveIteratorIterator = new \RecursiveIteratorIterator(
+                $recursiveDirectoryIterator,
+                \RecursiveIteratorIterator::CHILD_FIRST
+            );
+            $this->_inodes = $recursiveIteratorIterator;
+        }
+        catch(\Exception $e) {
+            var_dump($e);
+        }
+    }
+
+    public function getNodes() {
+        return $this->_inodes;
+    }
+
+    public function count() {
+        $this->getNodes()->rewind();
+        return iterator_count($this->getNodes());
+    }
+
+    protected function recursiveDirs() {
+        try {
+            $recursiveDirectoryIterator = new \RecursiveDirectoryIterator(
+                $this->_baseDir,
+                \FilesystemIterator::SKIP_DOTS
+            );
+            $recursiveIteratorIterator = new \RecursiveIteratorIterator(
+                $recursiveDirectoryIterator,
+                \RecursiveIteratorIterator::CHILD_FIRST
+            );
+        }
+        catch(\Exception $e) {
+            var_dump($e);
+        }
+        $recursiveIteratorIterator->rewind();
+        return $recursiveIteratorIterator;
+    }
+
+    protected function flatDir() {
+        try {
+            $inodes = new \DirectoryIterator($this->_baseDir);
+        }
+        catch(\Exception $e) {
+            var_dump($e);
+        }
+        $inodes->rewind();
+        return $inodes;
+    }
+
+    public function getFiles() {
+
+    }
+
+    public function getDirs() {
+
+    }
 
 
 
-use LibreMVC\Web\Instance\PathsFactory\Path;
+}
 
-// Theme & application mimimum
-//$base = Path::processPattern($config->Pattern,$config->Tokens);
-//var_dump($base);
+class File {
 
-//$appPaths = PathsFactory::processPattern(array_merge($config->Pattern, $config->Root), $config->Tokens);
-//var_dump($appPaths);
+    use Modifiable;
+    use isDistantFile;
 
-$intance = (array)PathsFactory::processPattern(array_merge($config->Pattern, $config->Root, $config->Instances), $config->Tokens);
-//unset($intance['sites']);
-//var_dump($intance);
+}
 
-$path = new Path($intance, 'http://www.libre.dev/','/home/inwebo/www/demo/');
+class InodesFilter extends \FilterIterator {
 
-var_dump($path->getBaseUrl());
-var_dump($path->getBaseDir());
-//var_dump($path);
-//var_dump($config);
-//var_dump($config2);
-/*
-$node = new \LibreMVC\Files\Inode($ini);
-//var_dump($node);
-$node = new \LibreMVC\Files\Inode("benchmark.php");
-$node->copy(".benchmark.php");*/
-//var_dump($node->getMimeType());
+    public function accept() {
 
-//var_dump(PathsFactory::inject($appPaths,'http://www.google.fr/'));
-//$appPaths = PathsFactory::inject($appPaths,'http://www.google.fr/');
+    }
+
+}
 
 
+$dir = new _Directory( './demo/io/' );
+$dir->getNodes()->rewind();
+while($dir->getNodes()->valid()) {
+    $node = $dir->getNodes()->current();
+    echo($node->getBaseName() . "<br>");
+    $dir->getNodes()->next();
+}
+echo $dir->count();
+//\LibreMVC\Files\IO::mkDir($writable,"test",0755);
 
-//var_dump($path->getBaseUrl());
-//var_dump($path->getBaseDir());
-//var_dump($path->getBaseDir("core"));
+// Objet->cp('blab/bla/bla');
 
-$base = 'hello';
-$ref =& $base;
-$copy = $ref;
+// IO::cp($who, $where);
 
-$copy = 'world';
-
-echo $base;
+$v = parse_ini_file('./demo/config2.ini',true);
+var_dump($v);
