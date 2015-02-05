147930f07dae75bcc9bc567d1ce9140ede5a8378
inwebo <inwebo@gmail.com>
2015-02-05 22:36:12 +0100

    Fix routes

--- modules/bookmarks/autoload.php
+++ modules/bookmarks/autoload.php
@@ -1,13 +1,14 @@
 <?php
-registerModule();
-addRoute('bookmarks/[:action]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController','index');
-addRoute('bookmarks/page/[:id|page]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController','index');
-addRoute('bookmarks/tag/[:id|tag]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController','tag');
-addRoute('bookmarks/tags', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController','tags');
-addRoute('bookmarks/api/public[/][:action]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\AjaxBookmarkController','index');
-addRoute('bookmarks/api/private', '\\LibreMVC\\MVC\\Controller\\AjaxController\\PrivateAjaxController','index');
-addRoute('bookmarks/api/rest', '\\LibreMVC\\MVC\\Controller\\AjaxController\\PrivateAjaxController\\RestController','index');
-
+namespace LibreMVC\Modules\Bookmarks  {
+    registerModule();
+    addRoute('/[:action]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController', 'index');
+    addRoute('page/[:id|page]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController', 'index');
+    addRoute('tag/[:id|tag]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController', 'tag');
+    addRoute('tags', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\BookmarksController', 'tags');
+    addRoute('api/public[/][:action]', '\\LibreMVC\\Modules\\Bookmarks\\Controllers\\AjaxBookmarkController', 'index');
+    addRoute('api/private', '\\LibreMVC\\MVC\\Controller\\AjaxController\\PrivateAjaxController', 'index');
+    addRoute('api/rest', '\\LibreMVC\\MVC\\Controller\\AjaxController\\PrivateAjaxController\\RestController', 'index');
+}
 // Une route pour le formulaire, ajax privé
 
 // Une route pour ajouter un bookmark, rest privé
