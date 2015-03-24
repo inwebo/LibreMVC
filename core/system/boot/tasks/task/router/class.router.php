fce96c7c85f4c0e56b3666103060faf19c00701d
inwebo <inwebo@gmail.com>
2015-03-24 16:16:10 +0100

    Validate route

--- core/system/boot/tasks/task/router/class.router.php
+++ core/system/boot/tasks/task/router/class.router.php
@@ -2,6 +2,7 @@
 
 namespace LibreMVC\System\Boot\Tasks\Task {
 
+    use LibreMVC\Routing\RouterError404;
     use LibreMVC\System\Boot\Tasks\Task;
     use LibreMVC\System\Hooks;
     use LibreMVC\Routing\Router as RoutesRouter;
@@ -29,9 +30,29 @@ namespace LibreMVC\System\Boot\Tasks\Task {
                 self::ROUTE_CONSTRAINT
             );
             $default = RoutesCollection::get(('error'))->getDefaultRoute();
+            //var_dump($default);
             $router->attachDefaultRoute($default);
-            self::$_routed = $router->dispatch();
+            try {
+                self::$_routed = $router->dispatch();
+            }
+            catch(\Exception $e) {
+                self::$_exceptions[] = $e;
+                //throw $e;
+            }
             return self::$_routed;
+        }
+
+        protected function validateRouteController() {
+            if( !is_null(self::$_routed) ) {
+                // Est un controller valide
+                if( !class_exists(self::$_routed->controller) ) {
+                    self::$_exceptions[] = new RouterError404("Unknown controller " . self::$_routed->controller . ", check typo or create the needed file.");
+                    //throw new RouterError404("Unknown controller " . self::$_routed->controller . ", check typo or create the needed file.");
+                }
+            }
+            else {
+                //throw new RouterError404();
+            }
         }
 
         protected function end() {
