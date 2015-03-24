aa90358532c76860008efbe9ca5c89dd6029a407
inwebo <inwebo@gmail.com>
2015-03-24 16:20:40 +0100

    Add Http status code support

--- modules/error/controllers/errorcontroller/class.errorcontroller.php
+++ modules/error/controllers/errorcontroller/class.errorcontroller.php
@@ -3,18 +3,29 @@
 namespace LibreMVC\Modules\Error\Controllers {
 
     use LibreMVC\Mvc\Controller\StaticController;
+    use LibreMVC\System;
+    use LibreMVC\Http\Request;
+    use LibreMVC\View;
 
     class ErrorController extends StaticController{
 
+        /**
+         * @var int HTTP status code
+         */
+        protected $_httpCode;
+
         const GET = '\\LibreMVC\\Modules\\Error\\Controllers\\ErrorController';
+
+        public function __construct(Request $request, View $view, $baseDir, $httpCode) {
+            parent::__construct($request, $view, $baseDir);
+            $this->_baseDir = $baseDir;
+        }
 
         public function init(){
             parent::init();
-            $viewsPath = $this->_system->this()->getModuleBaseDirs('error','static_views') . $this->getViewFileFormat();
-            $this->changePartial('body',$viewsPath);
-        }
-
-        public function __call( $name, $arguments ) {
+            //$viewsPath = System::this()->getModuleBaseDirs('error','static_views') . $this->getViewFileFormat();
+            //$viewsPath = $this->_baseDir . 'error.php';
+            //$this->changePartial('body',$viewsPath);
             $this->render();
         }
     }
