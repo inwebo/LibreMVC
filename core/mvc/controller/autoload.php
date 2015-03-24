dd8d350338aa7b15e088b4a57221fb1e64ca7cd8
inwebo <inwebo@gmail.com>
2015-03-24 16:10:16 +0100

    Add traits support

--- core/mvc/controller/autoload.php
+++ core/mvc/controller/autoload.php
@@ -1,6 +1,10 @@
 <?php
+include('traits/staticview/class.staticview.php');
+include('traits/database/class.database.php');
+
 include('icontroller/class.icontroller.php');
 include('basecontroller/class.basecontroller.php');
+include('actioncontroller/class.actioncontroller.php');
 include('class.controller.php');
 include('ajaxcontroller/class.ajaxcontroller.php');
 include('ajaxcontroller/privateajaxcontroller/class.privateajaxcontroller.php');
