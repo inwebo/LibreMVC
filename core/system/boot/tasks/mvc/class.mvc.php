a4e98f2ae19e072a28812c43abe36198c77896fb
inwebo <inwebo@gmail.com>
2015-03-24 16:11:47 +0100

    Add Exceptions support

--- core/system/boot/tasks/mvc/class.mvc.php
+++ core/system/boot/tasks/mvc/class.mvc.php
@@ -9,10 +9,11 @@ namespace LibreMVC\System\Boot\Tasks {
     use LibreMVC\System\Boot\Tasks\Task\Modules;
     use LibreMVC\System\Boot\Tasks\Task\Layout;
     use LibreMVC\System\Boot\Tasks\Task\Router;
-    use LibreMVC\System\Boot\Tasks\Task\Body;
+    use LibreMVC\System\Boot\Tasks\Task\Exceptions;
     use LibreMVC\System\Boot\Tasks\Task\FrontController;
     use LibreMVC\System\Boot\Tasks\Task\Session;
     use LibreMVC\System\Boot\Tasks\Task\Themes;
+    use LibreMVC\System\Boot\Tasks\Task\Body;
 
     /**
      * Class MVC
@@ -24,16 +25,18 @@ namespace LibreMVC\System\Boot\Tasks {
     class MVC extends Tasks {
         function __construct($config) {
             $this->_name = "MVC";
-            $this->attach( new Init($config) );
-            $this->attach( new Paths() );
-            $this->attach( new Instance() );
-            $this->attach( new Modules() );
-            $this->attach( new Themes() );
-            $this->attach( new Session() );
-            $this->attach( new Layout() );
-            $this->attach( new Router() );
-            $this->attach( new Body() );
-            $this->attach( new FrontController() );
+            $this->attach(new Init($config));
+            $this->attach(new Paths());
+            $this->attach(new Instance());
+            $this->attach(new Modules());
+            $this->attach(new Themes());
+            $this->attach(new Session());
+            $this->attach(new Layout());
+            $this->attach(new Router());
+            $this->attach(new Body());
+            $this->attach(new Exceptions());
+            $this->attach(new FrontController());
+            $this->attach(new Exceptions());
         }
     }
 }
