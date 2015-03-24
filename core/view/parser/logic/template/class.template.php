c50cd23836819263ac105d3fdb0bed16b93cd45b
inwebo <inwebo@gmail.com>
2015-03-24 16:17:59 +0100

    Typo

--- core/view/parser/logic/template/class.template.php
+++ core/view/parser/logic/template/class.template.php
@@ -1,74 +1,46 @@
 <?php
-namespace LibreMVC\View\Parser\Logic;
-
-/**
- * LibreMVC
- *
- * LICENCE
- *
- * You are free:
- * to Share ,to copy, distribute and transmit the work to Remix —
- * to adapt the work to make commercial use of the work
- *
- * Under the following conditions:
- * Attribution, You must attribute the work in the manner specified by
- * the author or licensor (but not in any way that suggests that they
- * endorse you or your use of the work).
- *
- * Share Alike, If you alter, transform, or build upon
- * this work, you may distribute the resulting work only under the
- * same or similar license to this one.
- *
- *
- * @category   LibreMVC
- * @package    View
- * @subpackage Template
- * @subpackage Logic
- * @copyright Copyright (c) 2005-2012 Inwebo (http://www.inwebo.net)
- * @license   http://http://creativecommons.org/licenses/by-nc-sa/3.0/
- * @version   $Id:$
- * @link      https://inwebo@github.com/inwebo/My.Sessions.git
- * @since     File available since Beta 01-01-2012
- */
-
-/**
- * Class métier à appliqué sur un Tag Tpl.
- *
- * Permet d'inclure un fichier template qui sera lui même parsé.
- * 
- * <code>
- * {tpl="data.tpl"}
- * </code>
- *
- * @category   LibreMVC
- * @package    View
- * @subpackage Template
- * @copyright  Copyright (c) 1793-2222 Inwebo Veritas (http://www.inwebo.net)
- * @license    http://framework.zend.com/license   BSD License
- * @version    $Id:$
- * @link       https://github.com/inwebo/Template
- * @since      File available since Beta
- */
-use LibreMVC\View\Parser\Logic;
-use LibreMVC\View\Parser;
-
-class Template extends Logic {
-
+namespace LibreMVC\View\Parser\Logic {
     /**
-     * Inclus et Parse un fichier template.
-     * 
-     * @param array $match Un tableau de retour de preg_match_all
-     * @return string Le contenu fichier template modifié par une fonction pcre
+     * Class métier à appliqué sur un Tag Tpl.
+     *
+     * Permet d'inclure un fichier template qui sera lui même parsé.
+     *
+     * <code>
+     * {tpl="data.tpl"}
+     * </code>
+     *
+     * @category   LibreMVC
+     * @package    View
+     * @subpackage Template
+     * @copyright  Copyright (c) 1793-2222 Inwebo Veritas (http://www.inwebo.net)
+     * @license    http://framework.zend.com/license   BSD License
+     * @version    $Id:$
+     * @link       https://github.com/inwebo/Template
+     * @since      File available since Beta
      */
-    public function process($match) {
-        try {
-            // Nouveau parser
-            $_layout = new \LibreMVC\View\Template($match[1]);
-            $temp = new Parser($_layout, $this->dataProvider );
-        } catch (\Exception $e) {
-            var_dump($e);
-        }
-        return $temp->getContent();
-    }
+    use LibreMVC\View\Parser\Logic;
+    use LibreMVC\View\Parser;
+    use LibreMVC\View\Template as Tpl;
 
+    class Template extends Logic {
+
+        /**
+         * Inclus et Parse un fichier template.
+         *
+         * @param array $match Un tableau de retour de preg_match_all
+         * @return string Le contenu fichier template modifié par une fonction pcre
+         * @throws \Exception
+         */
+        public function process($match) {
+            try {
+                // Nouveau parser
+                $_layout = new Tpl($match[1]);
+                $temp = new Parser($_layout, $this->dataProvider );
+            } catch (\Exception $e) {
+                throw $e;
+            }
+            return $temp->getContent();
+        }
+
+    }
 }
