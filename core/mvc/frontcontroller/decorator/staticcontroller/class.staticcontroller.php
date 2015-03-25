b3c42a08068b3f92babc4a6925d85f2243df4964
inwebo <inwebo@gmail.com>
2015-03-25 19:13:16 +0100

    Step

--- core/mvc/frontcontroller/decorator/staticcontroller/class.staticcontroller.php
+++ core/mvc/frontcontroller/decorator/staticcontroller/class.staticcontroller.php
@@ -1,34 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inwebo
- * Date: 23/03/15
- * Time: 15:40
- */
+namespace LibreMVC\Mvc\FrontController\Decorator {
 
-namespace LibreMVC\Mvc\FrontController\Decorator;
+    use LibreMVC\Mvc\Controller;
+    use LibreMVC\Mvc\FrontController\Decorator;
 
+    class StaticController extends Decorator{
 
-use LibreMVC\Mvc\Controller;
-use LibreMVC\Mvc\FrontController\Decorator;
+        public function isValidAction() {
+            return true;
+        }
 
-class StaticController extends Decorator{
-
-    const TYPE = '\\LibreMVC\Mvc\\Controller\\StaticController';
-
-    public function isValidAction() {
-        return true;
-    }
-
-    public function isTyped() {
-        return is_a($this->_controller, static::TYPE, true);
-    }
+        public function isTyped() {
+            return is_a($this->_controller, $this->getType(), true);
+        }
 
 
-    public function factory($args=array()){
-        $class = new \ReflectionClass($this->_controller);
-        $instance = $class->newInstanceArgs($args);
-        $action = Controller::getActionShortName($this->_action);
-        return $instance->__call($action, $this->_params);
+        public function factory($args=array()){
+            $class = new \ReflectionClass($this->_controller);
+            $instance = $class->newInstanceArgs($args);
+            $action = Controller::getActionShortName($this->_action);
+            return $instance->__call($action, $this->_params);
+        }
     }
 }
