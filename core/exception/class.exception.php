3a803aefcb7fe373cd900871963820d5af2e81cb
inwebo <inwebo@gmail.com>
2015-03-25 19:08:28 +0100

    Typo

--- core/exception/class.exception.php
+++ core/exception/class.exception.php
@@ -3,7 +3,7 @@
 namespace LibreMVC {
 
     class Exception extends \Exception{
-        public function __construct($message = null, $code = 0, Exception $previous = null){
+        public function __construct($message = null, $code = 0, \Exception $previous = null){
             parent::__construct($message, $code, $previous);
         }
     }
