e20eec752ddad8e365e11fbb668a2c5735c1deb2
inwebo <inwebo@gmail.com>
2015-03-25 19:12:46 +0100

    Refactoring

--- modules/error/controllers/errorcontroller/class.errorcontroller.php
+++ modules/error/controllers/errorcontroller/class.errorcontroller.php
@@ -4,29 +4,7 @@ namespace LibreMVC\Modules\Error\Controllers {
 
     use LibreMVC\Mvc\Controller\StaticController;
     use LibreMVC\System;
-    use LibreMVC\Http\Request;
     use LibreMVC\View;
 
-    class ErrorController extends StaticController{
-
-        /**
-         * @var int HTTP status code
-         */
-        protected $_httpCode;
-
-        const GET = '\\LibreMVC\\Modules\\Error\\Controllers\\ErrorController';
-
-        public function __construct(Request $request, View $view, $baseDir, $httpCode) {
-            parent::__construct($request, $view, $baseDir);
-            $this->_baseDir = $baseDir;
-        }
-
-        public function init(){
-            parent::init();
-            //$viewsPath = System::this()->getModuleBaseDirs('error','static_views') . $this->getViewFileFormat();
-            //$viewsPath = $this->_baseDir . 'error.php';
-            //$this->changePartial('body',$viewsPath);
-            $this->render();
-        }
-    }
+    class ErrorController extends StaticController{}
 }
