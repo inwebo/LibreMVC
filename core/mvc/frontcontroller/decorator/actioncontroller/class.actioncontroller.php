ab08cce3ff820a4c8ac33db12d0becd6b3e55a41
inwebo <inwebo@gmail.com>
2015-03-25 19:13:02 +0100

    Step isTyped()

--- core/mvc/frontcontroller/decorator/actioncontroller/class.actioncontroller.php
+++ core/mvc/frontcontroller/decorator/actioncontroller/class.actioncontroller.php
@@ -1,23 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inwebo
- * Date: 23/03/15
- * Time: 15:40
- */
+namespace LibreMVC\Mvc\FrontController\Decorator {
 
-namespace LibreMVC\Mvc\FrontController\Decorator;
+    use LibreMVC\Mvc\FrontController\Decorator;
+    use LibreMVC\View;
 
+    class ActionController extends Decorator{
 
-use LibreMVC\Http\Request;
-use LibreMVC\Mvc\FrontController\Decorator;
-use LibreMVC\View;
-
-class ActionController extends Decorator{
-
-    const TYPE = '\\LibreMVC\Mvc\\Controller\\ActionController';
-
-    public function isTyped() {
-        return is_subclass_of($this->_controller, static::TYPE, true);
+        public function isTyped() {
+            return is_subclass_of($this->_controller,$this->getType(), true);
+        }
     }
 }
