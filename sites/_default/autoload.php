0663aebfa9d3fa88a0305041c72e96a964c0503b
inwebo <inwebo@gmail.com>
2015-03-26 18:21:17 +0100

    Add demo routes

--- sites/_default/autoload.php
+++ sites/_default/autoload.php
@@ -1,4 +1,6 @@
 <?php
 registerInstance();
 addRoute('[:action]','\\LibreMVC\\Controllers\\HomeController','index');
+addRoute('private/[:action]','\\LibreMVC\\Controllers\\PrivateController','index');
+addRoute('ajax/','\\LibreMVC\\Controllers\\HomeAjaxController','index');
 addStaticRoute('static/[:static]');
