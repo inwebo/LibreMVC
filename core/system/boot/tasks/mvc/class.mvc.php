90697600214840aeb31091d39dbf3470c630d51d
inwebo <inwebo@gmail.com>
2015-03-26 18:23:55 +0100

    Add comments

--- core/system/boot/tasks/mvc/class.mvc.php
+++ core/system/boot/tasks/mvc/class.mvc.php
@@ -25,16 +25,27 @@ namespace LibreMVC\System\Boot\Tasks {
     class MVC extends Tasks {
         function __construct($config) {
             $this->_name = "MVC";
+            // Load config file
             $this->attach(new Init($config));
+            // Prepare whole app paths
             $this->attach(new Paths());
+            // Choose instance
             $this->attach(new Instance());
+            // Load modules
             $this->attach(new Modules());
+            // Load assets
             $this->attach(new Themes());
+            // Prepare User
             $this->attach(new Session());
+            // Prepare main View layout
             $this->attach(new Layout());
+            // Routing
             $this->attach(new Router());
+            // Prepare body partial
             $this->attach(new Body());
+            // Controller factory
             $this->attach(new FrontController());
+            // Exception System
             $this->attach(new Exceptions());
         }
     }
