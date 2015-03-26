61ae6d1238aa71c694a7a069020fdcf7c13c3cc4
inwebo <inwebo@gmail.com>
2015-03-26 18:23:27 +0100

    Refactoring

--- core/mvc/controller/authcontroller/class.authcontroller.php
+++ core/mvc/controller/authcontroller/class.authcontroller.php
@@ -1,64 +1,25 @@
 <?php
 namespace LibreMVC\Mvc\Controller {
 
+    class UnauthorizedException extends \Exception {
+        protected $code = 500;
+        const MSG = 'Unautorised, please login !';
+    }
+
     use LibreMVC\Modules\AuthUser\Models\AuthUser;
+    use LibreMVC\Mvc\Controller;
+    use LibreMVC\Http\Request;
+    use LibreMVC\View;
 
-    class AuthController extends BaseController{
-        /**
-         * @var AuthUser
-         */
-        protected $_sessionUser;
-        /**
-         * @var array Nom des roles autorisés, null si tous
-         */
-        protected $_roles;
-        /**
-         * @var array Id des permissions autorisées, null si toutes
-         */
-        protected $_permissions;
+    class AuthController extends ActionController{
 
-        protected function init() {
-            parent::init();
-            $this->_sessionUser = (isset($_SESSION['User'])) ? $_SESSION['User'] : null;
-            //$this->validateRequest();
-        }
+        use Traits\Authentification;
 
-        protected function validateRequest() {
-            if( !$this->validateRoles() ) {
-                return false;
-            }
-            else {
-                if( !$this->validatePerms() ) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        protected function validateRoles() {
-            if( is_null($this->_roles) ) {
-                return true;
-            }
-            elseif(is_array($this->_roles)) {
-                $valid = true;
-                foreach( $this->_roles as $role ) {
-                    $valid = $valid & $this->_sessionUser->is($role);
-                }
-                return $valid;
-            }
-        }
-
-        protected function validatePerms() {
-            if( is_null($this->_permissions) ) {
-                return true;
-            }
-            elseif(is_array($this->_permissions)) {
-                $valid = true;
-                foreach( $this->_permissions as $perm ) {
-                    $valid =& $this->_sessionUser->hasPermission($perm);
-                }
-                return $valid;
-            }
+        public function __construct( Request $request, View $view, AuthUser $authUser ) {
+            $this->_request = $request;
+            $this->_view    = $view;
+            $this->setAuthUser($authUser);
+            $this->init();
         }
 
     }
