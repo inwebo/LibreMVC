bf93f1f1521802cfc03a6240b2438254a1d9f8a9
inwebo <inwebo@gmail.com>
2015-03-26 18:21:28 +0100

    Add demo routes

--- sites/_default/views/home/index.php
+++ sites/_default/views/home/index.php
@@ -1,3 +1,5 @@
 <h2>Demo</h2>
 <p><a href="static/demo">Static file</a></p>
 <p><a href="je-suis-une-route-inconnue/">Route inconnue</a></p>
+<p><a href="private/">Route privée</a></p>
+<p><a href="ajax/">Route ajax</a></p>
