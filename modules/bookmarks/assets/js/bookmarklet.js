8195dd04a7c3849a60bb7ad2dd37dce4a6517967
inwebo <inwebo@gmail.com>
2015-04-02 13:59:17 +0200

    Step

--- modules/bookmarks/assets/js/bookmarklet.js
+++ modules/bookmarks/assets/js/bookmarklet.js
@@ -4,7 +4,7 @@ javascript:(function(){
 
     user            = '<?php echo $this->user ?>';
     publicKey       = '<?php echo $this->publicKey ?>';
-    restService     = '<?php echo $this->form ?>';
+    restService     = '<?php echo $this->restService ?>';
     xpathFavicon    = document.evaluate('//*[contains(@rel,\'shortcut icon\')]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     timestamp       = Date.now();
     bookmark        = {
@@ -17,16 +17,13 @@ javascript:(function(){
     bookmark.url    = encodeURIComponent(location.href);
     bookmark.title  = encodeURIComponent(document.title);
     bookmark.favicon= (xpathFavicon.snapshotLength != 0) ? encodeURIComponent(xpathFavicon.snapshotItem(0).getAttribute('href'))  : null;
-
-    console.log(bookmark);
-
     metas = document.getElementsByTagName('meta');
     var y = metas.length;
     for (var x=0 ; x<y; x++) {
-        if (metas[x].name.toLowerCase() == 'description') {
+        if (metas[x].name.toLowerCase() === 'description') {
             bookmark.description = encodeURIComponent(metas[x].content);
         }
-        if (metas[x].name.toLowerCase() == 'keywords') {
+        if (metas[x].name.toLowerCase() === 'keywords') {
             bookmark.tags = encodeURIComponent(metas[x].content);
         }
     }
@@ -39,7 +36,7 @@ javascript:(function(){
         '&title=' + bookmark.title +
         '&description=' + bookmark.description +
         '&tags=' + bookmark.tags +
-        '&favicon='+bookmark.favicon,
+        '&favicon='+ bookmark.favicon,
             'Save me !',
             'location=0,titlebar=0,toolbar=0,menubar=0,resizable=0,width=300,height=550,left=0,top=0'
     ).focus();
