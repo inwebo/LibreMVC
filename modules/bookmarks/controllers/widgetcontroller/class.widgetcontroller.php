f55b971d9c675eb831a44fa3a1fc18d3e3642431
inwebo <inwebo@gmail.com>
2015-04-02 14:01:56 +0200

    Add WidgetController

---
+++ modules/bookmarks/controllers/widgetcontroller/class.widgetcontroller.php
@@ -0,0 +1,30 @@
+<?php
+namespace LibreMVC\Modules\Bookmarks\Controllers {
+
+    use LibreMVC\Http\Url;
+    use LibreMVC\Mvc\Controller\AjaxController\PrivateAjaxController;
+    use LibreMVC\Controllers\PrivateController;
+
+    class WidgetController extends PrivateController{
+
+
+        public function init() {
+            parent::init();
+            $this->setSystem(\LibreMVC\System::this());
+        }
+
+        public function bookmarkletAction() {
+            var_dump(Url::getServer(true,true) . 'form/');
+            $body = $this->getSystem()->getModule('bookmarks')->getStaticViews('dir') . self::getActionShortName('bookmarkletAction') . '.php';
+            $bookmarklet = $this->getSystem()->getModule('bookmarks')->getJs("dir") . self::getActionShortName('bookmarkletAction') . '.js';
+            $this->changePartial('body',$body);
+            $this->getView()->getPartial('body')->attachPartial('bookmarklet',$bookmarklet);
+            $this->toView('user', user()->login);
+            $this->toView('publicKey', user()->publicKey);
+            $this->toView('restService', Url::getServer(true,true) . 'form/');
+            $this->render();
+        }
+
+    }
+
+}
