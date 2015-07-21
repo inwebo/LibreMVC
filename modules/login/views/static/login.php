006da079bd3f41b5740e4a45a6d832aa35e02d38
inwebo <inwebo@gmail.com>
2015-07-21 21:27:33 +0200

    Step

--- modules/login/views/static/login.php
+++ modules/login/views/static/login.php
@@ -3,9 +3,12 @@
         <h2 class="form-signin-heading">Please sign in</h2>
         <input name="user" type="text" placeholder="Email address" class="form-control">
         <input name="password" type="password" placeholder="Password" class="form-control">
+        <!--
         <label class="checkbox">
             <input type="checkbox" value="remember-me"> Remember me
         </label>
+        -->
+        <br>
         <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
     </form>
 <?php } else {?>
